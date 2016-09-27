d7afec1fdbccbb648d6375f0d357fa63e1d5b804
mihail1orlov <mihail_orlov@hotmail.com>
2016-09-27 10:32:50 +0300

    array remove

--- CW/003/003_Array/Program.cs
+++ CW/003/003_Array/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _003_Array
 {
@@ -6,12 +7,25 @@ namespace _003_Array
 	{
 		static void Main()
 		{
-			//var list = new List<string>() {"1", "2", "3"};
-			var list = "12345678901234567890";
-			for (int i = 0, j = 0; i < list.Length; i++, j--)
+			var list = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+			//var list = "12345678901234567890";
+			for (int i = 0, j = 0; i < list.Count; i++, j--)
 			{
-				Console.ForegroundColor = (ConsoleColor) (i%15);
+				if (i == 4)
+				{
+					list.Remove(list[i]);
+				}
+				Console.ForegroundColor = (ConsoleColor)(i % 15);
 				Console.Write(list[i]);
+			}
+
+			foreach (var item in list)
+			{
+				if (item == "5")
+				{
+					//list.Remove(item);
+				}
+				Console.WriteLine(item);
 			}
 
 			var user = new User();
