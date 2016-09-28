e3eb95d9f72c86a9560e8e4055ccb21d695dbf10
mihail1orlov <mihail_orlov@hotmail.com>
2016-09-28 21:13:13 +0300

    Array

---
+++ CW/004/04_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04_Array
+{
+	class Program
+	{
+		static void Main()
+		{
+			try
+			{
+				var strings = new string[3];
+				if (strings[0] == null)
+				{
+					((string)null).Split(' ');
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("NullReferenceException" + e);
+			}
+		}
+	}
+}
