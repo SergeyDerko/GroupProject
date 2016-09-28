b589fa4ce04f7a2cf4fbfb972a008319f6032031
voooo <voooo@DESKTOP-H76077E>
2016-09-28 20:11:34 +0300

    Calc

---
+++ Calc/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите первое число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите оператор");
+            string i = Console.ReadLine();
+            double xy = 0;
+            if (i == "+")
+            {
+                xy = x + y;
+            }
+            else if (i == "-")
+            {
+                xy = x - y;
+            }
+            else if (i == "*")
+            {
+                xy = x * y;
+            }
+            else if (i == "/")
+            {
+                xy = x / y;
+            }
+            else
+            {
+                Console.WriteLine("что то пошло не так");
+            }
+            Console.WriteLine(value: xy);
+
+
+        }
+    }
+}
