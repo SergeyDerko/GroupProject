c2310322f4dfc936468ba129d503b5e2a634e6f8
voooo <voooo@DESKTOP-H76077E>
2016-09-28 20:52:44 +0300

    ADD

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
         static void Main(string[] args)
         {
             Console.WriteLine("введите первое число");
-            int x = Convert.ToInt32(Console.ReadLine());
+            double x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("введите второе число");
-            int y = Convert.ToInt32(Console.ReadLine());
+            double y = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("введите оператор");
             string i = Console.ReadLine();
             double xy = 0;
@@ -30,15 +30,14 @@ using System.Threading.Tasks;
             }
             else if (i == "/")
             {
-                xy = x / y;
+                xy = x/y;
             }
             else
             {
                 Console.WriteLine("что то пошло не так");
             }
             Console.WriteLine(value: xy);
-
-
+            //Console.File
         }
     }
 
