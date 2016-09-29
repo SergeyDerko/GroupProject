967a7220871b28a56972a651d0235e8d2897bd1a
@Pikhota <s.pikhota@gmail.com>
2016-09-29 09:51:29 +0300

    rename 01_parityNumber.cs refactoring code at Program.cs

--- 09_Sergei/01_parityNumber/Program.cs
+++ 09_Sergei/01_parityNumber/Program.cs
@@ -2,22 +2,19 @@
 
 namespace _01_parityNumber
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             bool finish = false;
             while (!finish)
                 try
                 {
                     Console.WriteLine("Entering number for check: ");
-
                     var number = Console.ReadLine();
-
-
-                    if (!string.IsNullOrEmpty(number) && isNum(number))
+                    if (!string.IsNullOrEmpty(number) && IsNum(number))
                     {
-                        var result = Int32.Parse(number) % 2 == 0 ? "is even" : "is odd";
+                        var result = int.Parse(number) % 2 == 0 ? "is even" : "is odd";
                         Console.WriteLine(result);
                         finish = true;
                     }
@@ -33,23 +30,16 @@ namespace _01_parityNumber
             Console.ReadKey();
         }
 
-        public static bool isNum(string number)
+        public static bool IsNum(string number)
         {
-            char[] anyString = number.ToCharArray();
+            var anyString = number.ToCharArray();
             for (var i = 0; i <= anyString.Length - 1; i++)
             {
-                if (Char.IsDigit(anyString[i]))
+                if (char.IsDigit(anyString[i]))
                 {
                     continue;
                 }
-                else if (anyString[i] == anyString.Length - 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return anyString[i] == anyString.Length - 1;
             }
             return true;
         }
