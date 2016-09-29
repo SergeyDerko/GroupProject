6b2c66df8fcd8723d8f0f1169e100281a6f38f43
@Pikhota <s.pikhota@gmail.com>
2016-09-29 14:34:47 +0300

    replace and refactoring

--- 09_Sergei/02_MoneySystem/Program.cs
+++ 09_Sergei/02_MoneySystem/Program.cs
@@ -2,11 +2,11 @@
 
 namespace MoneySystem
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            bool finish = false;
+            var finish = false;
             while (!finish)
             {
                 Console.WriteLine("Entering pounds: ");
@@ -15,23 +15,16 @@ namespace MoneySystem
                 var shilling = Console.ReadLine();
                 Console.WriteLine("Entering pens: ");
                 var pens = Console.ReadLine();
-                
-                if (CheckMoney(pound) == true && CheckMoney(shilling) == true &&
-                    CheckMoney(pens) == true)
-                {
-                    OldMoneySystem moneySystem = new OldMoneySystem(Decimal.Parse(pound), Decimal.Parse(shilling), Decimal.Parse(pens));
-                    Console.WriteLine("Old UK money system: " + pound + '.' + shilling + '.' + pens);
-                    moneySystem.GetNewSystemMoney();
-                    finish = true;
-                }
-                else
-                {
-                    finish = false;
-                }
+                if (!CheckMoney(pound) || !CheckMoney(shilling) || !CheckMoney(pens)) continue;
+                var moneySystem = new OldMoneySystem(decimal.Parse(pound), decimal.Parse(shilling),
+                    decimal.Parse(pens));
+                Console.WriteLine("Old UK money system: " + pound + '.' + shilling + '.' + pens);
+                moneySystem.GetNewSystemMoney();
+                finish = true;
             }
             Console.ReadLine();
         }
-        
+
         internal class OldMoneySystem
         {
             public decimal Pound { get; set; }
@@ -42,38 +35,43 @@ namespace MoneySystem
 
             public OldMoneySystem(decimal pound, decimal shilling, decimal pens)
             {
-                this.Pound = pound;
-                this.Shilling = shilling;
-                this.Pens = pens;
+                Pound = pound;
+                Shilling = shilling;
+                Pens = pens;
             }
             internal void GetNewSystemMoney()
             {
                 Pound *= 240;
                 Shilling *= 12;
-                var deflation = 2.4M;
+                const decimal deflation = 2.4M;
                 var money = Pens + Shilling + Pound;
                 var result = (money / 100) / deflation;
                 Console.WriteLine("New UK money system: " + result);
-            }   
+            }
         }
         public static bool CheckMoney(string money)
         {
-            char[] anyString = money.ToCharArray();
-            for (var i = 0; i <= anyString.Length - 1; i++)
+            if (!string.IsNullOrEmpty(money))
             {
-                if (Char.IsDigit(anyString[i]))
-                {
-                    continue;
-                }
-                else if (anyString[i] == anyString.Length - 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+
+                    var anyString = money.ToCharArray();
+                    for (var i = 0; i <= anyString.Length - 1; i++)
+                    {
+                        if (char.IsDigit(anyString[i]))
+                        {
+                        }
+                        else if (anyString[i] == anyString.Length - 1)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    } 
             }
+            else
+            { return false;}
             return true;
         }
     }
