6596b948dff095d27cab17f502c72f18a9920472
Serhiy <s.pikhota@gmail.com>
2016-09-30 18:52:57 +0300

    ADD 04_GeneretionOfExamples

---
+++ 09_Sergei/04_GeneretionOfExamples/Example.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _04_GeneretionOfExamples
+{
+    public class Example
+    {
+        //public Random Ran1 { get; } = new Random();
+        //public Random Ran2 { get; } = new Random();
+        public int Number1 { get; }
+        public int Number2 { get; }
+
+        internal Example(int range1, int range2)
+        {
+           // Ran1.Next(range1);
+           // Ran2.Next(range2);
+            Number1 = range1;
+            Number2 = range2;
+        }
+
+        /*internal Example(int rangeMin1, int rangeMax1, int rangeMin2, int rangeMax2 )
+        {
+           Number1 = Ran1.Next(rangeMin1,rangeMax1);
+           Number2 = Ran2.Next(rangeMin2, rangeMax2);
+        }*/
+
+        public void Next()
+        {
+            var rand = new Random();
+            var action = rand.Next(1,4);
+            var result = 0;
+            switch (action)
+            {
+                case 1:
+                    result = Number1 + Number2;
+                    Console.WriteLine("Action plus: " + result);
+                    break;
+                case 2:
+                    result = Number1 - Number2;
+                    Console.WriteLine("Action minus: " + result);
+                    break;
+                case 3:
+                    result = Number1 * Number2;
+                    Console.WriteLine("Action multiplication: " + result);
+                    break;
+                case 4:
+                    result = Number1/Number2;
+                    Console.WriteLine("Action division: " + result);
+                    break;
+            }
+        }
+    }
+}
---
+++ 09_Sergei/04_GeneretionOfExamples/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace _04_GeneretionOfExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввести первое число: ");
+            var number1 = int.Parse(Console.ReadLine());
+            Console.Write("Ввести второе число: ");
+            var number2 = int.Parse(Console.ReadLine());
+            var example = new Example(number1, number2);
+            example.Next();
+        }
+    }
+}
