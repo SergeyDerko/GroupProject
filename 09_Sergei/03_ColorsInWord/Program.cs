677d7dd396150a205caded13966aaa0789587ecf
Serhiy <s.pikhota@gmail.com>
2016-09-30 11:18:17 +0300

    ADD 03_ColorsInWord

---
+++ 09_Sergei/03_ColorsInWord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ColorsInWord
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var rand = new Random();
+            Console.WriteLine("Entering word: ");
+            var word = Console.ReadLine();
+            if (word != null)
+            {
+                var letters = word.ToCharArray();
+                for (var i = 0; i <= letters.Length - 1; i++)
+                {
+                    Console.ForegroundColor = (ConsoleColor)rand.Next(15) + 1;
+                    Console.Write(letters[i]);
+                    Thread.Sleep(500);
+                }
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadLine();
+        }
+    }
+}
