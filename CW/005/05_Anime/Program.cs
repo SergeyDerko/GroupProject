b2ebe9cd441116931c0fe1c9f03b65c13874c798
mihail1orlov <mihail_orlov@hotmail.com>
2016-10-01 10:17:10 +0300

    Console anime sample

---
+++ CW/005/05_Anime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _05_Anime
+{
+	class Program
+	{
+		static void Main()
+		{
+			for (int i = 0, j = Console.WindowWidth; i < Console.WindowWidth; i++, j--)
+			{
+				for (int k = 0; k < i; k++)
+				{
+					Console.Write(" ");
+				}
+				Console.Write("**");
+				Console.WriteLine();
+				for (int k = 0; k < i; k++)
+				{
+					Console.Write(" ");
+				}
+				Console.Write("**");
+				Thread.Sleep(100);
+				Console.Clear();
+			}
+			Console.ReadKey();
+		}
+	}
+}
