9a511540430afe6ad0056fd79c1e65a494582080
mihail1orlov <mihail_orlov@hotmail.com>
2016-10-04 02:48:42 +0300

    fix для Евгений Захаревич

--- 05_Eugene/ArreyGitVo/Program.cs
+++ 05_Eugene/ArreyGitVo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ArreyGitVo
@@ -12,23 +13,26 @@ namespace ArreyGitVo
         {
             int lenght = 8;
             int hight = 4;
-            int [][] arrey = new int [hight] [];
+            int[][] arrey = new int[hight][];
             for (int i = 0; i < arrey.Length; i++)
             {
-                arrey = new int[i][];
+                var random = new Random();// -1
+                arrey[i] = new int[lenght];
                 for (int j = 0; j < hight; j++)
-               {
-                arrey [i][j] = 0;
-               }
+                {
+                    arrey[i][j] = random.Next(0, 9);
+                    Thread.Sleep(20);
+                }
             }
             for (int i = 0; i < arrey.Length; i++)
             {
-                arrey = new int[i][];
+                //arrey = new int[i][]; -1
                 for (int j = 0; j < hight; j++)
                 {
-                    Console.Write(arrey[i][j] );
+                    Console.Write(arrey[i][j]);
                 }
                 Console.WriteLine();
             }
         }
+    }//-1
 }
