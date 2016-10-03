f66ca06606c7fc06300e5c34aad10495f25ae2f6
Serhiy <s.pikhota@gmail.com>
2016-10-03 19:49:19 +0300

    ADD 09_Arrays

---
+++ 09_Sergei/09_Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace _07_SortArrays
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Enter height: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter width: ");
+            int width = int.Parse(Console.ReadLine());
+            int[,] array = new int[height, width];
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    array[i, j] = rand.Next(100);
+                    Console.Write(" " + array[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
