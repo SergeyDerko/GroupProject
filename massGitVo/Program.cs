bdc9eaeeaf6a6a2e325ebe0204ff2ca033a6bbd1
voooo <voooo@DESKTOP-H76077E>
2016-10-03 19:59:04 +0300

    arrey

---
+++ massGitVo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace massGitVo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hight = 10;
+            int lenght = 5;
+            int[][] arrey = new int[hight][];
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                arrey[i] = new int[lenght];
+                for (int j = 0; j < arrey[i].Length; j++)
+                {
+                    arrey[i][j] = new Random().Next(0, 9);
+                    Thread.Sleep(50);
+                }
+            }
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                for (int j = 0; j < arrey[i].Length; j++)
+                {
+                    Console.Write(arrey[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
