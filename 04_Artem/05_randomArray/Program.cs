2406870cb169f7677967458ac4359c27f4ff09a2
artemanikeiev <artemanikeiev@gmail.com>
2016-10-03 19:22:06 +0300

    Add files via upload
    update (вывод))

--- 04_Artem/05_randomArray/Program.cs
+++ 04_Artem/05_randomArray/Program.cs
@@ -2,34 +2,37 @@
 
 namespace _05_randomArray
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             Console.WriteLine("Enter the length:");
             var x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the height:");
             var y = Convert.ToInt32(Console.ReadLine());
-            Random rand = new Random();
+            var rand = new Random();
 
-            int [,] table = new int[x, y];
+            var table = new int[x, y];
 
-            int sum = 0;
+            var sum = 0;
 
-            for (int i = 0; i < x; i++)
-                for (int j = 0; j < y; j++)
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
                 {
                     table[i, j] = rand.Next(0, 100);
-                    Console.WriteLine("{0}\t", table[i, j]); 
                     // выводим рандомные элементы от 0 до 100 (например)
+                    Console.Write("{0}\t", table[i, j]);
+
                     sum += table[i, j];
                 }
+                Console.WriteLine();
+            }
 
             Console.WriteLine("Sum of this array is " + sum);
-            
+
 
             Console.ReadKey();
         }
-        
-        }
+    }
 }
