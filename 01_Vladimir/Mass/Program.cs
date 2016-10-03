76b364e0ed9b96bb8083faf6b4e8d6e3bb02e7e5
@Pikhota <s.pikhota@gmail.com>
2016-10-03 20:50:43 +0300

    ADD 09_arrays

---
+++ 01_Vladimir/Mass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int [][] AR=new int[][];
+            //for (int i = 0; i <8; i++)
+            //{
+                
+            //}
+            //    var AR=New Int[][];
+
+        }
+    }
+}
