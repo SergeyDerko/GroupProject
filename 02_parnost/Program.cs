15af88b61e6edc4884bbd716b0fbcfc85bf0ad5e
romakholodetskiy <romakholodetskiy@gmail.com>
2016-10-04 08:52:54 +0300

    remove 02_parnist

--- 02_parnost/Program.cs
+++ 02_parnost/Program.cs
@@ -1,26 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02_parnost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Write("Введите число для проверки на парность=>");
             int k =int.Parse(Console.ReadLine());
-            if (k%2 == 1)
-            {
-                Console.WriteLine("Число непарное");
-            }
-            else
-            {
-                Console.WriteLine("Число парное");
-            }
+            Console.WriteLine(k%2 == 1 ? "Число непарное" : "Число парное");
             Console.ReadKey();
         }
     }
