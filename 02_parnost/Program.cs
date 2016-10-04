39aa7cb1cac302ac605ce75b599d8b19c7d3b0b6
romakholodetskiy <romakholodetskiy@gmail.com>
2016-10-04 08:45:25 +0300

    Add parnist project

---
+++ 02_parnost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_parnost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для проверки на парность=>");
+            int k =int.Parse(Console.ReadLine());
+            if (k%2 == 1)
+            {
+                Console.WriteLine("Число непарное");
+            }
+            else
+            {
+                Console.WriteLine("Число парное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
