fd435597a2bd92cd943481f27fd3fd9f16d1263f
yurik <yurik.matviychuk@gmail.com>
2016-10-04 16:03:34 +0300

    Add 06_projects

---
+++ 06_Jury/06_jaggedArray/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace _06_jaggedArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("\n ---Вывод зубчатого двумерного массива---" +
+                              "\n\n Задайте размерность массива!" +
+                              "\n Введите количество строк: ");
+            var row = Console.ReadLine();
+            int verify;
+            if (int.TryParse(row, out verify)&& !string.IsNullOrEmpty(row))
+            {
+                var random = new Random();
+                var x = int.Parse(row);
+                var random1 = new Random();
+                int y = 15;
+                var arr = new int[x, y];
+                Console.WriteLine($"\n Двумерный зубчатый массив рандомных чисел от 1 до 100, размерностью {row} " +
+                                  $"строк и рандомной длиной строки от 1 до 25: \n");
+                
+                for (var i = 0; i < x; i++)
+                {
+                    y = random1.Next(1, 15);
+                    for (var j = 0; j < y; j++)
+                    {
+                        arr[i, j] = random.Next(1, 100);
+                        Console.Write($" {arr[i, j]}\t ");
+                    }
+                    Console.WriteLine();
+                }
+
+               
+
+            }
+            else
+            {
+                Console.WriteLine(" Вы ввели некоректное значение!");
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
