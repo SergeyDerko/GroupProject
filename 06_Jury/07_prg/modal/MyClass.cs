4e13b5e9b253552052d3c6632ad36ccc517cd740
yurik <yurik.matviychuk@gmail.com>
2016-10-05 21:14:24 +0300

    update

--- 06_Jury/07_prg/modal/MyClass.cs
+++ 06_Jury/07_prg/modal/MyClass.cs
@@ -2,8 +2,11 @@
 
 namespace _07_prg.modal
 {
-    class MyClass:BaseClass
+    class MyClass:BaseClass,IMyClass
     {
-        
+        public string show()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
