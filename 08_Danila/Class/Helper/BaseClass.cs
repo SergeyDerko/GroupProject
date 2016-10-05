843f3828628cd81455249f48741d3e736f5e0500
Daniel_Akoltsev <daniel.akoltsev@yandex.ru>
2016-10-05 20:05:08 +0300

    fixxxx

--- 08_Danila/Class/Helper/BaseClass.cs
+++ 08_Danila/Class/Helper/BaseClass.cs
@@ -1,7 +1,9 @@
-﻿namespace Class
+﻿namespace Class.Helper
 {
     public class BaseClass
     {
-        
+        public string Name { get; set; }
+        public string SName { get; set; }
+        public string Age { get; set; }
     }
 }
