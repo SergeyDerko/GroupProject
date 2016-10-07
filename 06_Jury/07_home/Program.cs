25c5b25eaf2685276b5b09215413965927b8bfef
yurik <yurik.matviychuk@gmail.com>
2016-10-07 15:53:30 +0300

    Refactoring

--- 06_Jury/07_home/Program.cs
+++ 06_Jury/07_home/Program.cs
@@ -11,10 +11,10 @@ namespace _07_home
             var student = new IStudent() { Name = "Den" };
             var worker = new IWorker() { Name = "Nik" };
 
-            Console.WriteLine($"A am {gamer.Name}. {gamer.WhatIdo()}");
-            Console.WriteLine($"A am {school.Name}. {school.WhatIdo()}");
-            Console.WriteLine($"A am {student.Name}. {student.WhatIdo()}");
-            Console.WriteLine($"A am {worker.Name}. {worker.WhatIdo()}");
+            Console.WriteLine($"I am {gamer.Name}. {gamer.WhatIdo()}");
+            Console.WriteLine($"I am {school.Name}. {school.WhatIdo()}");
+            Console.WriteLine($"I am {student.Name}. {student.WhatIdo()}");
+            Console.WriteLine($"I am {worker.Name}. {worker.WhatIdo()}");
 
             
             Console.ReadLine();
