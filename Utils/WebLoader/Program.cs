b4ba6bca18c5ab501260131c17d1ede5ef097bc8
mihail <mihail_orlov@hotmail.com>
2016-10-10 11:44:33 +0300

    Add WebLoad

---
+++ Utils/WebLoader/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.IO;
+using System.Net;
+
+namespace WebLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+            foreach (var item in lines)
+            {
+                using (var client = new WebClient())
+                {
+                    string fileName = item.Substring(item.LastIndexOf("/") + 1);
+                    client.DownloadFile(item, fileName);
+                }
+            }
+        }
+    }
+}
