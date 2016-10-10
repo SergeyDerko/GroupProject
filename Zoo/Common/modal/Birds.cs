832522037db9a052c9c55056ed4d0eed844ab2b4
yurik <yurik.matviychuk@gmail.com>
2016-10-10 21:56:44 +0300

    update

--- Zoo/Common/modal/Birds.cs
+++ Zoo/Common/modal/Birds.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Common.modal
 {
-    class Birds
+    class Birds: IAnimal
     {
+        BirdsRace Birdsrace { get; set; }
+        public string Id { get; set; }
+        public TypeAnimal Type { get; set; }
+        public string Nickname { get; set; }
+        public int Age { get; set; }
+        public Sex Sex { get; set; }
+
+        private enum BirdsRace
+        {
+           Raven,
+           Dove,
+           Falcon
+        }
     }
 }
--- Zoo/Common/modal/Mammals.cs
+++ Zoo/Common/modal/Mammals.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Common.modal
 {
-    class Mammals
+    class Mammals: IAnimal
     {
+        MammalsRace MammalsRace { get; set; }
+        public string Nickname { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public TypeAnimal Type { get; set; }
+        public Sex Sex { get; set; }
     }
+
+    internal enum MammalsRace
+    {
+        Wolf,
+        Bear
+
+    }
+
 }
