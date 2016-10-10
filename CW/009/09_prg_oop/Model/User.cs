744bc60620edb996a3c8cf4aba0c2979c2199484
mihail <mihail_orlov@hotmail.com>
2016-10-10 23:48:50 +0300

    modification

---
+++ CW/009/09_prg_oop/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace _09_prg_oop.Model
+{
+    internal class User : IUser
+    {
+        public string Name { get; set; }
+        public string SName { get; set; }
+        public int Age { get; set; }
+    }
+}
