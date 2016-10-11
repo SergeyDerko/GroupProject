f7590c8f159f461ef5cc946d6378f3febd554ef4
yurik <yurik.matviychuk@gmail.com>
2016-10-11 19:41:36 +0300

    refacroring

--- 06_Jury/08_zoo/Program.cs
+++ 06_Jury/08_zoo/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _08_zoo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+
+
         }
     }
 }
