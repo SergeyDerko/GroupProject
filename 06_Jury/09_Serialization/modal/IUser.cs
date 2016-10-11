8a7035e90dfbe3b3aa68f1f60635bd2480751b44
yurik <yurik.matviychuk@gmail.com>
2016-10-11 19:15:24 +0300

    add projects

---
+++ 06_Jury/09_Serialization/modal/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace _09_Serialization.modal
+{
+    internal interface IUser
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+        string SName { get; set; }
+    }
+}
