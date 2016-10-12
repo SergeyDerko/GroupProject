89c764b178ba73390cf1e2fc236eaa1964f51313
mihail <mihail_orlov@hotmail.com>
2016-10-12 17:34:07 +0300

    test

--- CW/009/09_prg_oop/Program.cs
+++ CW/009/09_prg_oop/Program.cs
@@ -12,6 +12,7 @@ namespace _09_prg_oop
             //var json = JsonConvert.SerializeObject(new User());File.WriteAllText("out.json", json); 
             var r = "11";
             var t = 11;
+            var str = new string('*', 20);
             File.WriteAllText("out.json", json); 
         }
     }
