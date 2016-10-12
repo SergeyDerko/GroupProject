6e498537f68ab69a626b31f9be306cd4e62dcb0e
artemanikeiev <artemanikeiev@gmail.com>
2016-10-12 20:23:48 +0300

    add zoo

---
+++ 04_Artem/10_zoo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Common.modal;
+using Newtonsoft.Json;
+
+namespace _10_zoo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var animals = new List<IAnimal>
+            {
+                new Mammals(),
+                new Birds(),
+                new Reptiles(),
+                new Fish(),
+                new Amphibian()
+            };
+            var jsonE = JsonConvert.SerializeObject(animals);
+            var path = "out.json";
+            File.WriteAllText(path, null);
+            File.AppendAllText(path, jsonE);
+
+            var text = File.ReadAllText(path);
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+        }
+    }
+}
