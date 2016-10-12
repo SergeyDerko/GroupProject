88d9e2beae6c3697c14ca8010121067ec3e0be9b
voooo <voooo@DESKTOP-H76077E>
2016-10-12 20:21:03 +0300

    fix

--- 05_Eugene/_Zoo_vo/Program.cs
+++ 05_Eugene/_Zoo_vo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.modal;
+
 
 namespace _Zoo_vo
 {
@@ -10,6 +12,12 @@ namespace _Zoo_vo
     {
         static void Main(string[] args)
         {
+            var animals = new List<IAnimal>
+            {
+                new Mammals(),
+                new Birds()
+
+            };
         }
     }
 }
