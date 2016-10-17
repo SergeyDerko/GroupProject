1fe0ea716331f35a4fe4eefdb561933232b02e01
KefkaDM <eugene.iosifov@gmail.com>
2016-10-17 19:52:28 +0300

    Plitki add

---
+++ 10_KDMEugene/11_10_Plitochki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11_10_Plitochki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write
+             ("Plitochka size: ");
+
+            var plitochka = int.Parse(Console.ReadLine());
+
+            var size = plitochka * plitochka;
+
+            Console.Write
+            ("Height: ");
+
+            var height = int.Parse(Console.ReadLine());
+
+            Console.Write
+                ("Length: ");
+
+            var length = int.Parse(Console.ReadLine());
+
+            var stenochkaSize = height * length;
+
+            var plitkaNumber = stenochkaSize / size;
+            Console.Write("We need" + " " + plitkaNumber + " " + "plitok");
+            Console.ReadKey();
+        }
+    }
+}
