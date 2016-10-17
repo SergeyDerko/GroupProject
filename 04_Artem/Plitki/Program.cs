ed4ffbad78ff958acdb06123a8c44c5497b526da
artemanikeiev <artemanikeiev@gmail.com>
2016-10-17 20:02:44 +0300

    fix

--- 04_Artem/Plitki/Program.cs
+++ 04_Artem/Plitki/Program.cs
@@ -7,20 +7,20 @@ namespace Plitki
         static void Main()
         {
             Console.WriteLine("Введите ширину доски:");
-            var x = Convert.ToInt32(Console.ReadLine());
+            var L = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите высоту доски:");
-            var y = Convert.ToInt32(Console.ReadLine());
+            var H = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите ширину одной плиточки:");
             var a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите высоту одной плиточки:");
             var b = Convert.ToInt32(Console.ReadLine());
 
-            var s = x*y;
+            var s = L*H;
             var s1 = a*b;
             var res = s/s1;
 
-            Console.Write ("На доске шириной " + x);
-            Console.Write (" и высотой " + y);
+            Console.Write ("На доске шириной " + L);
+            Console.Write (" и высотой " + H);
             Console.Write(" можно разместить " + res);
             Console.Write(" плиток размерами " + a);
             Console.Write(" на " + b);
