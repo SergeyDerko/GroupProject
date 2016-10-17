02ad958d5c8a367dfcdf5cfcc87d449f2bb993b0
OLEG <OLEG@OLEG-PC>
2016-10-17 20:02:31 +0300

    new

---
+++ Plitochnik/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Plitochnik.Stena;
+
+namespace Plitochnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ps = 10;
+            //var ps = 
+            var pv = 10;
+            var ss = 100;
+            var sv = 100;
+            Stena stroyka = new Stena(ps, pv, ss, sv);
+            Console.WriteLine("\tШирина плитки: {0}\n" + "\tВысота плитки: {1}\n" + "\tШирина стены: {2}\n" + "\tВысота стены: {3}\n" +
+                              "\tКоличество плитки: {4}", stroyka.Pshirina, stroyka.Pvisota, stroyka.Sshirina, stroyka.Svisota, stroyka.Kolplit());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Plitochnik/Stena.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plitochnik
+{
+    class Stena
+    {
+        public int Pshirina;
+        public int Pvisota;
+        public int Sshirina;
+        public int Svisota;
+
+        public Stena(int ps, int pv, int ss, int sv)
+        {
+            Pshirina = ps;
+            Pvisota = pv;
+            Sshirina = ss;
+            Svisota = sv;
+        }
+
+        public int SposhPlitka()
+        {
+            return Pshirina*Pvisota;
+        }
+
+        public int SposhStena()
+        {
+            return Sshirina*Svisota;
+        }
+
+        public int Kolplit()
+        {
+            return SposhStena()/SposhPlitka();
+        }
+    }
+}
