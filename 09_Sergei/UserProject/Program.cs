87b4903ae1ba473bb9ee856c9d4f715b100cb33f
@Pikhota <s.pikhota@gmail.com>
2016-10-17 10:39:17 +0300

    update UserProject

--- 09_Sergei/UserProject/Program.cs
+++ 09_Sergei/UserProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.ComponentModel;
 using UserProject.Characters;
 
 namespace UserProject
@@ -8,23 +9,25 @@ namespace UserProject
     {
         private static void Main()
         {
-            var r = new Recruit();
-            Console.WriteLine("Recruit Lvl is: "+r.Lvl+"\n"+"Recruit Exp: "+r.Exp);
+            var r = new Recruit {Exp = 100};
+            Console.WriteLine("Recruit Lvl: "+r.Lvl+"\n"+"Recruit Exp: "+r.Exp);
             Console.WriteLine();
-            
+            r.LevelUp();
+            Console.ReadKey();
+
             var w = new Warrior {Exp = 200};
             w.LevelUp();
             
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8}",
-                "Str is: " + w.Str + "\n",
-                "Agi is: " + w.Agi + "\n",
-                "Int is: " + w.Int + "\n",
-                "Health is: " + w.Health + "\n",
-                "Mana is: " + w.Mana + "\n",
-                "Attack is: " + w.Attack + "\n",
-                "Defence is: " + w.Defence+"\n",
-                "Exp is: " + w.Exp + "\n",
-                "Lvl is: " + w.Lvl);
+                "Str: " + w.Str + "\n",
+                "Agi: " + w.Agi + "\n",
+                "Int: " + w.Int + "\n",
+                "Health: " + w.Health + "\n",
+                "Mana: " + w.Mana + "\n",
+                "Attack: " + w.Attack + "\n",
+                "Defence: " + w.Defence+"\n",
+                "Exp: " + w.Exp + "\n",
+                "Lvl: " + w.Lvl);
             Console.ReadKey();
         }
     }
