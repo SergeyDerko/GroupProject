42b8bb5c13802c2d8ebc750df8791265fd34b21c
mihail <mihail_orlov@hotmail.com>
2016-10-19 18:16:07 +0300

    Add UserInfo

--- CW/012/012_try/Program.cs
+++ CW/012/012_try/Program.cs
@@ -7,13 +7,41 @@ namespace _012_try
         static void Main()
         {
             UncheckedPrg();
-            var user = new User
-            {
-                Name = "John",
-                SName = "Smit",
-                Age = 25
-            };
+            StructPrg();
+            // Создадим делегат
+            DelegatePrg();
+        }
 
+        private static void DelegatePrg()
+        {
+            var calc = new Calc(Operation.Sum);
+            var calculation = calc.Calculation(2, 3);
+        }
+
+        private int Test(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static void StructPrg()
+        {
+            UserInfo user1 = new UserInfo("Alexandr", 26);
+            Console.Write("user1: ");
+            user1.WriteUserInfo();
+            UserInfo user2 = new UserInfo("Elena", 22);
+            Console.Write("user2: ");
+            user2.WriteUserInfo();
+
+            // Показать главное отличие структур от классов
+            user1 = user2;
+            user2.Name = "Natalya";
+            user2.Age = 25;
+            Console.Write("\nuser1: ");
+            user1.WriteUserInfo();
+            Console.Write("user2: ");
+            user2.WriteUserInfo();
+
+            Console.ReadLine();
         }
 
         private static void UncheckedPrg()
--- CW/012/012_try/User.cs
+++ CW/012/012_try/User.cs
@@ -1,9 +1,23 @@
-﻿namespace _012_try
-{
+﻿using System;
+
+namespace _012_try
+{// Создадим структуру
+
+
     internal class User
     {
         public string Name { get; set; }
         public string SName { get; set; }
         public int Age { get; set; }
+
+        public static User NewUser()
+        {
+            return new User
+            {
+                Age = new Random().Next(18, 100),
+                Name = Guid.NewGuid().ToString("N").ToUpper(),
+                SName = Guid.NewGuid().ToString("N").ToUpper(),
+            };
+        }
     }
 }
