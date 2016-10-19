130a41a7cf37482f87320833b64a62f4e2e8c41e
romakholodetskiy <romakholodetskiy@gmail.com>
2016-10-19 13:03:19 +0300

    q

--- 05_array_int/Program.cs
+++ 05_array_int/Program.cs
@@ -10,6 +10,22 @@ namespace _05_array_int
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество колонок=>");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество строк=>");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array=new int[50,50];
+            Random ran = new Random();
+            for (int i = 0; i < n; i++)
+            { 
+                for (int j = 0; j < m; j++)
+                {
+                    array[i,j] = ran.Next(1,9);
+                    Console.Write(array[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
