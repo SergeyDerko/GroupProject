0846dfe0b57c37f76a76b0b2c5ebe45bd637420d
OLEG <OLEG@OLEG-PC>
2016-10-19 19:57:16 +0300

    new

---
+++ 03_Oleg/13_F/Filey/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Filey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C: \Users\OLEG\Desktop\new\WriteText.txt");
+
+            System.Console.WriteLine(" \t \n{0}", text);
+            var s = new StringBuilder();
+            var n = new StringBuilder();
+            var w = new StringBuilder();
+            foreach (var m in text)
+            {
+                if (char.IsSymbol(m))
+                {
+                    s.Append(m);
+                }
+                else if (char.IsNumber(m))
+                {
+                    n.Append(m);
+                }
+                else if (char.IsLetter(m))
+                {
+                    w.Append(m);
+                }
+            }
+            Console.WriteLine(s.ToString() + n + w);
+            File.WriteAllText(@"C: \Users\OLEG\Desktop\new\WriteText.txt", s.ToString() + n + w);
+
+            Console.ReadLine();
+        }
+    }
+}
