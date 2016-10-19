f137843bd957718b7ff4a4d2b9674d8f98c9594c
artemanikeiev <artemanikeiev@gmail.com>
2016-10-19 20:59:38 +0300

    new

---
+++ 04_Artem/12_FileRead/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _12_FileRead
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var text = File.ReadAllText(@"in.txt");
+            var symbol = new StringBuilder();
+            var number = new StringBuilder();
+            var letter = new StringBuilder();
+
+            foreach (var s in text)
+            {
+                if (char.IsSymbol(s))
+                {
+                    symbol.Append(s);  
+                }
+                else if (char.IsNumber(s))
+                {
+                    number.Append(s);
+                }
+                else if (char.IsLetter(s))
+                {
+                    letter.Append(s);
+                }
+                File.WriteAllText(@"out.txt",symbol.ToString()+ number + letter);
+            }
+           
+        }
+    }
+}
