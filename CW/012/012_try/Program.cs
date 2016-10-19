fc91defdb5d94c8946e09c390536f092486c195d
mihail <mihail_orlov@hotmail.com>
2016-10-19 17:42:11 +0300

    Add 11 lesson prg

---
+++ CW/012/012_try/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _012_try
+{
+    public class Program
+    {
+        static void Main()
+        {
+            UncheckedPrg();
+            var user = new User
+            {
+                Name = "John",
+                SName = "Smit",
+                Age = 25
+            };
+
+        }
+
+        private static void UncheckedPrg()
+        {
+            while (true)
+            {
+                Console.WriteLine("Программа 11 занятия");
+                try
+                {
+                    Console.Write("Введите a: ");
+                    // Используем unchecked в одном выражении
+                    var a = unchecked((byte) int.Parse(Console.ReadLine()));
+                    Console.Write("Введите b: ");
+                    var b = unchecked((byte) int.Parse(Console.ReadLine()));
+
+                    // Используем checked для всего блока операторов
+                    unchecked
+                    {
+                        var result = (byte) (a + b);
+                        Console.WriteLine("a + b = " + result);
+                        result = (byte) (a*b);
+                        Console.WriteLine("a*b = " + result + "\n");
+                    }
+
+                    // Используем checked для всего блока операторов
+                    checked
+                    {
+                        var result = (byte) (a + b);
+                        Console.WriteLine("a + b = " + result);
+                        result = (byte) (a*b);
+                        Console.WriteLine("a*b = " + result + "\n");
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.Write("Переполнение\n\n");
+                }
+                Console.WriteLine("Для выхода нажмите Escape");
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ CW/012/012_try/User.cs
@@ -0,0 +1,9 @@
+﻿namespace _012_try
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string SName { get; set; }
+        public int Age { get; set; }
+    }
+}
