82f2f1971bb1f717fc6b2af50571b70caf16704d
mihail <mihail_orlov@hotmail.com>
2016-10-20 07:02:10 +0300

    fix

--- 08_Danila/12_Fileread/Program.cs
+++ 08_Danila/12_Fileread/Program.cs
@@ -11,17 +11,17 @@ namespace _12_Fileread
     {
         static void Main(string[] args)
         {
-           var F= File.ReadAllText(@"in.txt");
-            var symbvol = new StringBuilder();
-            var number = new StringBuilder();
-            var letter = new StringBuilder();
-            foreach (var s in text)
-            {
-                if (char.IsSymbvol(s))
-                {
-                    symbvol.Append( )
-                }
-            }
+           //var F= File.ReadAllText(@"in.txt");
+           // var symbvol = new StringBuilder();
+           // var number = new StringBuilder();
+           // var letter = new StringBuilder();
+           // foreach (var s in text)
+           // {
+           //     if (char.IsSymbvol(s))
+           //     {
+           //         symbvol.Append( )
+           //     }
+           // }
         }
     }
 }
