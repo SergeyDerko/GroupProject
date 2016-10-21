04971cd625603d44121f8581a9925c8cf79c4d13
@Pikhota <s.pikhota@gmail.com>
2016-10-21 13:15:56 +0300

    update 10_Calc_Modul

--- 09_Sergei/10_Calc_Modul/Calculator.cs
+++ 09_Sergei/10_Calc_Modul/Calculator.cs
@@ -6,73 +6,9 @@ namespace _10_Calc_Modul
     internal class Calculator 
     {
 
-        public void Calc(string[] args)
+        public void Calc(string args)
         {
-        //    var pattern = @"";
-        //    try
-        //    {
-        //        Console.WriteLine("Press any key.");
-        //        while (Console.ReadKey().Key != ConsoleKey.Escape)
-        //        {
-        //            if (string.IsNullOrEmpty(args.ToString()))
-        //            {
-        //                throw new ArgumentNullException(nameof(args));
-        //            }
-
-        //            foreach (var arg in args)
-        //            {
-        //                var tArg = "";
-        //                var str = arg;
-        //                str = str.Replace(" ", string.Empty);
-        //                var temp = str.ToCharArray();
-        //                foreach (var t in temp)
-        //                {
-        //                    if (Regex.IsMatch(t.ToString(), @"\d") || Regex.IsMatch(t.ToString(), @"\s") || Regex.IsMatch(t.ToString(), @"[*/+-=]"))
-        //                    {
-        //                        tArg += t.ToString();
-        //                        if (Regex.IsMatch(t.ToString(), @"(\d+)"))
-        //                        {
-        //                            if (pattern.Contains(@"\d+") || !pattern.Contains(@"([*/+-])"))
-        //                            {
-        //                                pattern = @"(\d+)";
-        //                            }
-        //                            else
-        //                            {
-        //                                pattern += @"(\d+)";
-        //                            }
-        //                        }
-        //                        if (Regex.IsMatch(tArg, @"(\d+)([*/+-])(\d+)"))
-        //                        {
-        //                            var res = GetResult(pattern, tArg);
-        //                            tArg = res.ToString();
-        //                            pattern = @"(\d+)";
-        //                        }
-        //                        else if (Regex.IsMatch(t.ToString(), @"([*/+-])"))
-        //                        {
-        //                            pattern += @"([*/+-])";
-        //                        }
-        //                        else if (Regex.IsMatch(t.ToString(), @"[=]"))
-        //                        {
-        //                            pattern += @"[=]";
-        //                            break;
-        //                        }
-        //                    }
-        //                    else
-        //                    {
-                                
-        //                    }
-        //                }
-        //            }
-        //            pattern = @"";
-        //            Console.WriteLine("If you want close program press Esc. / Для закрытия программы нажмите Esc");
-        //        }
-        //        Console.Clear();
-        //    }
-        //    catch (FormatException ex)
-        //    {
-        //        Console.Write("Format exception: " + ex);
-        //        Console.ReadKey();
-        //    }
+        
         }
 
         protected  int GetResult(string pattern, string arg)
--- 09_Sergei/10_Calc_Modul/ScanDirectory.cs
+++ 09_Sergei/10_Calc_Modul/ScanDirectory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Text.RegularExpressions;
 using _10_Calc_Modul.Interfaces;
 
@@ -30,26 +29,27 @@ namespace _10_Calc_Modul
             var files = Directory.GetFiles(pathDirectory); // считываем все файлы с указанной директории
             foreach (var fileName in files) //проходим по каждому файлу отдельно
             {
-                /*if (fileName.IndexOfAny(Path.GetInvalidFileNameChars()) == -1)
+                if (fileName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
                 {
                     File.Delete(pathDirectory + fileName);
-                }*/ //это будущая проверка на корректное название файла, реализую чуть позже
+                } //это будущая проверка на корректное название файла, реализую чуть позже
                 var str = File.ReadAllText(fileName); // считываем данные с файла
-                var result = GetResult(str); //обрабатаваем данные в методе GetResult(str)
-                File.WriteAllText(input, str); //записываем обработанные данные
+                var result = DataProcessing(str); //обрабатаваем данные в методе DataProcessing(str)
+                File.WriteAllText(input, result); //записываем обработанные данные
             }
         }
         #endregion
-
-        public string GetResult(string str)
+        //Обработка полученных данных и возвращение результата обработки
+        public string DataProcessing(string str)
         {
             var pattern = "";
+
+            var expResult = "";
             var tempStr = str.ToCharArray();
             foreach (var x in tempStr)
             {
-                if (!Regex.IsMatch(x.ToString(), @"\d") && !Regex.IsMatch(x.ToString(), @"\s") &&
-                    !Regex.IsMatch(x.ToString(), @"[*/+-=]")) break;
-                str += x.ToString();
+                if (!Regex.IsMatch(x.ToString(), @"\d") && !Regex.IsMatch(x.ToString(), @"[*/+-=]")) continue;
+                expResult += x.ToString();
                 if (Regex.IsMatch(x.ToString(), @"(\d+)"))
                 {
                     if (pattern.Contains(@"\d+") || !pattern.Contains(@"([*/+-])"))
@@ -65,10 +65,10 @@ namespace _10_Calc_Modul
                         pattern += @"(\d+)";
                     }
                 }
-                if (Regex.IsMatch(str, @"(\d+)\s+([*/+-])\s+(\d+)"))
+                if (Regex.IsMatch(expResult, @"(\d+)\s+([*])\s+(\d+)"))
                 {
-                    var res = GetResult(pattern, str);
-                    str = res.ToString();
+                    var res = GetResult(pattern, expResult);
+                    expResult = res.ToString();
                     pattern = @"(\d+)";
                 }
                 else if (Regex.IsMatch(x.ToString(), @"([*/+-])"))
@@ -80,7 +80,7 @@ namespace _10_Calc_Modul
                     pattern = " ";
                 }
             }
-            return str;
+            return expResult;
         }
     }
 }
