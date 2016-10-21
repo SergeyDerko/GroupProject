0838c2a332e0fb618810a5dcb1b942e916fa9ffb
mihail <mihail_orlov@hotmail.com>
2016-10-21 18:12:26 +0300

    add project

---
+++ CW/402/402_Advanced_programming/Model/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _402_Advanced_programming.Model
+{
+    class Person : IPerson
+    {
+        public string Name { get; set; }
+        public string SName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    interface IEmployee
+    {
+        string EmployeeId { get; set; }
+        string Login { get; set; }
+        string Password { get; set; }
+    }
+
+    class Employee : Person, IEmployee
+    {
+        private sealed class EmployeeIdLoginPasswordEqualityComparer : IEqualityComparer<Employee>
+        {
+            public bool Equals(Employee x, Employee y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return string.Equals(x.EmployeeId, y.EmployeeId) && string.Equals(x.Login, y.Login) && string.Equals(x.Password, y.Password);
+            }
+
+            public int GetHashCode(Employee obj)
+            {
+                unchecked
+                {
+                    var hashCode = (obj.EmployeeId != null ? obj.EmployeeId.GetHashCode() : 0);
+                    hashCode = (hashCode*397) ^ (obj.Login != null ? obj.Login.GetHashCode() : 0);
+                    hashCode = (hashCode*397) ^ (obj.Password != null ? obj.Password.GetHashCode() : 0);
+                    return hashCode;
+                }
+            }
+        }
+
+        private static readonly IEqualityComparer<Employee> EmployeeIdLoginPasswordComparerInstance = new EmployeeIdLoginPasswordEqualityComparer();
+
+        public static IEqualityComparer<Employee> EmployeeIdLoginPasswordComparer
+        {
+            get { return EmployeeIdLoginPasswordComparerInstance; }
+        }
+
+        public string EmployeeId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
--- CW/402/402_Advanced_programming/Program.cs
+++ CW/402/402_Advanced_programming/Program.cs
@@ -1,10 +1,88 @@
-﻿namespace _402_Advanced_programming
+﻿// материал взят с ресурса http://professorweb.ru
+using System;
+using _402_Advanced_programming.Model;
+
+namespace _402_Advanced_programming
 {
     public class Program
     {
         static void Main()
         {
-            
+            Employee();
+            Additions();
+            Points();
+        }
+
+        private static void Employee()
+        {
+            var employee = new Employee();
+            Person person = null;
+            if (employee is Person)
+            {
+                person = (Person) employee;
+            }
+
+            var e = person as Employee;
+            var b = e.Equals(employee);
+            b.ToString();
+        }
+
+        private static void Points()
+        {
+            Point point1 = new Point(1, 12, -4);
+            Point point2 = new Point(0, -3, 18);
+            Console.WriteLine("Координаты первой точки: " +
+                 point1.x + " " + point1.y + " " + point1.z);
+            Console.WriteLine("Координаты второй точки: " +
+                point2.x + " " + point2.y + " " + point2.z + "\n");
+
+            Point point3 = point1 + point2;
+            Console.WriteLine("\nPoint1 + Point2 = "
+                + point3.x + " " + point3.y + " " + point3.z);
+            point3 = point1 - point2;
+            Console.WriteLine("Point1 - Point2 = "
+                + point3.x + " " + point3.y + " " + point3.z);
+            point3 = -point1;
+            Console.WriteLine("-Point1 = "
+                + point3.x + " " + point3.y + " " + point3.z);
+            point2++;
+            Console.WriteLine("Point2++ = " + point2.x + " " + point2.y + " " + point2.z);
+            point2--;
+            Console.WriteLine("Point2-- = " + point2.x + " " + point2.y + " " + point2.z);
+
+            if (point1 == point2)
+                Console.WriteLine("Объекты равны");
+
+            if (point1)
+                Console.WriteLine("Все координаты объекта myObject1 положительны");
+
+            if (point1 & point2)
+                Console.WriteLine("У объектов все координаты положительные");
+            else
+                Console.WriteLine("Есть отрицательные или равные нулю координаты");
+
+            if (!point2)
+                Console.WriteLine("Во втором объекте есть отрицательные координаты");
+
+            Console.ReadLine();
+        }
+
+        private static void Additions()
+        {
+            MyArray arr = new MyArray(5)
+            {
+                [0] = 5,
+                [1.235] = 12,
+                [2] = 3,
+                [3.045] = 18,
+                [3.9] = 1
+            };
+
+            Console.WriteLine("Получившийся массив: ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write("{0}\t", arr[i]);
+
+            Console.ReadLine();
         }
     }
 }
