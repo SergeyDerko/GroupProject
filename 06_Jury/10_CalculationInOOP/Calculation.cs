8ba5ce14494f6b9d9f98881a54d10b9a66cc989d
yurikhub <yurik.matviychuk@gmail.com>
2016-10-22 13:02:38 +0300

    refactoring calc

--- 06_Jury/10_CalculationInOOP/Calculation.cs
+++ 06_Jury/10_CalculationInOOP/Calculation.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lesson11
 {
-    
-    class Calculation
+    internal class Calculation
     {
         public readonly int Result;
 
--- 06_Jury/10_CalculationInOOP/Program.cs
+++ 06_Jury/10_CalculationInOOP/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace lesson11
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //Читаем файл
             var file = new FileInOut(@"../../Input.txt");
