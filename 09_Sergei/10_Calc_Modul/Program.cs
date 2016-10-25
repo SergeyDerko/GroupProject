d7097921da881d51bf049569594862c933e06788
@Pikhota <s.pikhota@gmail.com>
2016-10-25 13:08:12 +0300

    refactoring 10_Calc_Modul

--- 09_Sergei/10_Calc_Modul/Program.cs
+++ 09_Sergei/10_Calc_Modul/Program.cs
@@ -7,7 +7,39 @@ namespace _10_Calc_Modul
     {
         private static void Main()
         {
-            Start();
+            string tab2 = new string('\t', 2);
+            string tab5 = new string('\t',5);
+            startmenu:
+            Console.Write(new string('\t', 2) + "Программа для считывания с файлов данные в виде математических выражений и их просчета.\n" +
+            tab5 + "Меню:\n" +
+            tab5 + "1. Старт.\n" +
+            tab5 + "2. О программе.\n" +
+            tab5 + "3. Автор\n" +
+            tab5 + "4. Выход\n");
+            switch(Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    Start();//Метод старт программы        
+                    break;
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    Console.Write(tab2 + "Программа сканирует директорию, и обрабатывает все файлы в ней"+"\n"+tab2+
+                        ",находит там математическиевыражения считает их и сохраняет в определенный файл."+"\n"+tab2+
+                        " После чего удаляет файл с которым отработала из данной директории.\n");
+                    Console.ReadKey();
+                    break;
+                case ConsoleKey.D3:
+                    Console.Clear();
+                    Console.Write(tab5 + "Created by Pikhota Serhii."+"\n"+tab5+ "all right reserved "+'\n');
+                    Console.ReadKey();
+                    break;
+                case ConsoleKey.D4:
+                    break;
+                default:
+                    Console.Clear();
+                    goto startmenu;
+            }
+            
         }
         #region Start
         private static void Start()
