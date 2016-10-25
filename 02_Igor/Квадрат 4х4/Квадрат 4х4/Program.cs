a6952e929b5d8fcc6bec062bc4da01752705929a
IgorYarko <igor.iarko@gmail.com>
2016-10-25 23:32:25 +0300

    Add Квадрат 4х4

---
+++ 02_Igor/Квадрат 4х4/Квадрат 4х4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Квадрат_4х4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Виводимо назву програми
+            Console.WriteLine("Елемент шахматної дошки 2х2");
+            //просимо ввести користувача ширину та висоту
+            Console.WriteLine("Введiть ширину");
+            var x = 4; //int.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть довжину");
+            var y = 4; //int.Parse(Console.ReadLine());
+            var str = "*";
+            
+            for (var i = 0; i < x; i++)
+                Console.Write(str);
+            Console.WriteLine();
+
+            }
+    }
+}
