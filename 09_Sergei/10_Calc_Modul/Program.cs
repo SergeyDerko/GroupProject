a99bc337a5433858866ac95a7f622f7caf2c9140
@Pikhota <s.pikhota@gmail.com>
2016-10-25 16:24:31 +0300

    update 10_Calc_Modul

--- 09_Sergei/10_Calc_Modul/Program.cs
+++ 09_Sergei/10_Calc_Modul/Program.cs
@@ -7,8 +7,36 @@ namespace _10_Calc_Modul
     {
         private static void Main()
         {
+            StartMenu();
+        }
+        #region Start
+
+
+        private static void Start()
+        {
+
+            do
+            {
+                var s = new ScanDirectory();//интанцируем обьект класса ScanDirectory
+                s.Scan(s.PathDir);//Сканирует директорию/папку, обрабатывает файлы и т.д.:)
+                
+                var time = 10000;
+                var count = time/1000;
+                while (count!=0)
+                {
+                    count--;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Loading  " + count);
+                }
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);//Для выхода из программы нужно нажать Esc
+        }
+        #endregion
+        #region Options 
+        private static void StartMenu()
+        {
             string tab2 = new string('\t', 2);
-            string tab5 = new string('\t',5);
+            string tab5 = new string('\t', 5);
             startmenu:
             Console.Write(new string('\t', 2) + "Программа для считывания с файлов данные в виде математических выражений и их просчета.\n" +
             tab5 + "Меню:\n" +
@@ -16,41 +44,32 @@ namespace _10_Calc_Modul
             tab5 + "2. О программе.\n" +
             tab5 + "3. Автор\n" +
             tab5 + "4. Выход\n");
-            switch(Console.ReadKey().Key)
+            switch (Console.ReadKey().Key)
             {
                 case ConsoleKey.D1:
                     Start();//Метод старт программы        
                     break;
                 case ConsoleKey.D2:
                     Console.Clear();
-                    Console.Write(tab2 + "Программа сканирует директорию, и обрабатывает все файлы в ней"+"\n"+tab2+
-                        ",находит там математическиевыражения считает их и сохраняет в определенный файл."+"\n"+tab2+
+                    Console.Write(tab2 + "Программа сканирует директорию, и обрабатывает все файлы в ней" + "\n" + tab2 +
+                        ",находит там математическиевыражения считает их и сохраняет в определенный файл." + "\n" + tab2 +
                         " После чего удаляет файл с которым отработала из данной директории.\n");
                     Console.ReadKey();
+                    Console.Clear();
+                    goto startmenu;
                     break;
                 case ConsoleKey.D3:
                     Console.Clear();
-                    Console.Write(tab5 + "Created by Pikhota Serhii."+"\n"+tab5+ "all right reserved "+'\n');
+                    Console.Write(tab5 + "Created by Pikhota Serhii." + "\n" + tab5 + "all right reserved " + '\n');
                     Console.ReadKey();
-                    break;
+                    Console.Clear();
+                    goto startmenu;
                 case ConsoleKey.D4:
                     break;
                 default:
                     Console.Clear();
                     goto startmenu;
             }
-            
-        }
-        #region Start
-        private static void Start()
-        {
-            do
-            {
-                var s = new ScanDirectory();
-                s.Scan(s.PathDir);
-                Thread.Sleep(20000);
-            }
-            while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
         #endregion
     }
