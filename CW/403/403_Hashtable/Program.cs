203d3126524513009844cd7c1110c97a2c79f518
mihail <mihail_orlov@hotmail.com>
2016-10-26 17:12:40 +0300

    add 403

---
+++ CW/403/403_Hashtable/Program.cs
@@ -0,0 +1,28 @@
+﻿//пример взят с сайта http://professorweb.ru
+using System;
+using System.Collections;
+
+namespace _403_Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем хеш-таблицу
+            Hashtable ht = new Hashtable();
+
+            // Добавим несколько записей
+            ht.Add("alex85", "12345");
+            ht.Add("fg230404", "12ldsd");
+            ht.Add("I_best_user", "12349");
+
+            // Считаем коллекцию ключей
+            ICollection keys = ht.Keys;
+
+            foreach (string s in keys)
+                Console.WriteLine(s + ": " + ht[s]);
+
+            Console.ReadLine();
+        }
+    }
+}
