38bd6bee6ba795260e60cdfe5a11c4e25f58903a
voooo <voooo@DESKTOP>
2016-10-26 19:49:00 +0300

    fix

---
+++ 05_Eugene/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            string text = "";
+            for (int i = 0; i < 11;i++)
+                {
+                    text = Convert.ToString (Guid.NewGuid());
+                    File.WriteAllText(path, text);
+                };
+
+
+        }
+    }
+}
