9d208633d8b2235a82a72b84d3fa33f316def410
voooo <voooo@DESKTOP>
2016-10-28 21:18:33 +0300

    add

---
+++ 05_Eugene/calc new/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
