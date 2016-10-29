97a16627531c5f0453cafabce6432d81d57b60fa
@Pikhota <s.pikhota@gmail.com>
2016-10-29 17:16:03 +0300

    update

--- 09_Sergei/501_GenObjects/Program.cs
+++ 09_Sergei/501_GenObjects/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Newtonsoft.Json;
 namespace _501_GenObjects
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
