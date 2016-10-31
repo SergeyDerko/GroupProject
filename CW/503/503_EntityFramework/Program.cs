dd3ff7be829c0a3ad59a4e36a11499917d5a4387
mihail <mihail_orlov@hotmail.com>
2016-10-31 23:39:42 +0200

    start entity framework

--- CW/503/503_EntityFramework/Program.cs
+++ CW/503/503_EntityFramework/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace _503_EntityFramework
+﻿//материал взят с сайта http://professorweb.ru/my/entity-framework/6/level1/
+
+namespace _503_EntityFramework
 {
     class Program
     {
         static void Main()
         {
+
         }
     }
 }
