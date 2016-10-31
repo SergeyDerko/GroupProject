1a082e3d537d1ae045c46eb56a9c806b1d3720a0
mihail <mihail_orlov@hotmail.com>
2016-10-31 23:24:23 +0200

    fix

--- 05_Eugene/calc new/Program.cs
+++ 05_Eugene/calc new/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 
 namespace calc_new
@@ -20,7 +19,7 @@ namespace calc_new
         {
             User x = new User();
 
-            JsonConvert.
+            //JsonConvert.
 
 
         }
--- CW/502/502_CW/Program.cs
+++ CW/502/502_CW/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using _04_Array;
 
 namespace _502_CW
 {
