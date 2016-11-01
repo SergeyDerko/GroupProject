3612a8c89812cc628ab5796a1c0330aed2784217
Daniel_Akoltsev <daniel.akoltsev@yandex.ru>
2016-11-01 16:27:56 +0200

    tank

---
+++ 08_Danila/tank/Fire.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    class Fire
+    {
+        public Orientation Direction = new Orientation();
+        public Point Location;
+        public bool Shot;
+        public Image FireImage;
+
+        public void Move()
+        {
+            if (Shot)
+            {
+                if (Direction == Orientation.Up) { Location.Y -= 4; }
+                if (Direction == Orientation.Down) { Location.Y += 4; }
+                if (Direction == Orientation.Left) { Location.X -= 4; }
+                if (Direction == Orientation.Right) { Location.X += 4; }
+                if ((Location.X < 0) || (Location.X > 635) || (Location.Y < 0) || (Location.Y > 475)) Shot = false;
+            }
+        }
+    }
+}
---
+++ 08_Danila/tank/Gamer.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    enum Orientation { Up, Down, Left, Right }
+
+    class Gamer
+    {
+        public Orientation Ор = new Orientation();
+        public Point Location;
+        public bool Move;
+        public Image GamerImage;
+        
+        public void Go()
+        {
+            if (Move)
+            {
+                if (Ор == Orientation.Up){ if (Location.Y > 0) Location.Y -= 2; }
+                else
+                    if (Ор == Orientation.Down) { if (Location.Y < 443) Location.Y += 2; }
+                    else
+                        if (Ор == Orientation.Left) { if (Location.X > 0) Location.X -= 2; }
+                        else
+                            if (Ор == Orientation.Right) { if (Location.X < 603) Location.X += 2; }
+            }
+        }
+    }
+}
