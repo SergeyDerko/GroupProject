c88cae8a39f5b45442e86b76980ee3a8daa441bd
mihail <mihail_orlov@hotmail.com>
2016-11-01 10:50:12 +0200

    add customer

---
+++ cw/503/503_EntityFramework/Model/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _503_EntityFramework.Model
+{
+    public class Customer
+    {
+        // Определить поля, которые используются в базе данных
+        
+        public int IdCustomer { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
