7c0a6c2f15f83f617f59f08491a43670ebf27aba
@Pikhota <s.pikhota@gmail.com>
2016-11-02 20:59:36 +0200

    fix

--- 09_Sergei/503_EF/User.cs
+++ 09_Sergei/503_EF/User.cs
@@ -2,12 +2,11 @@
 
 namespace _503_EF
 {
-    class User
+    internal class User
     {
         [Key]
-        public int id { get; set; }
-        public string Name { get; set;}
-        public string SName { get; set;}
-        public int Age { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SName { get; set; }
     }
 }
