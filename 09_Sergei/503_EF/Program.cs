abae01529cd688b9af46497cd970f9dd7c6d680b
@Pikhota <s.pikhota@gmail.com>
2016-11-02 20:24:50 +0200

    update

---
+++ 09_Sergei/503_EF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _503_EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            user.Name = Guid.NewGuid().ToString("N").ToUpper();
+            user.SName = Guid.NewGuid().ToString("N").ToUpper();
+
+
+        }
+    }
+
+}
