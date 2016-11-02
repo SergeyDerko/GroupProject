f41b77a43b05e10d2479c195cc2ea929352eca95
OLEG <OLEG@OLEG-PC>
2016-11-02 19:39:13 +0200

    new

---
+++ 03_Oleg/18_Factor/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number;
+            var value = Console.ReadLine();
+            bool result = Int32.TryParse(value, out number);
+            if (result)
+            {
+                int truueer = number;
+                for (int i = 1; i < truueer; i++)
+                {
+
+                    number = number * i;
+                }
+            }
+            else
+            {
+                Console.WriteLine("failed");
+            }
+            Console.WriteLine("Factorial {0} is {1}", value, number);
+            Console.ReadLine();
+        }
+    }
+}
