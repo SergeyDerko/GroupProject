d36d619f952d466c84a19e3348bc0604a370ae12
Daniel_Akoltsev <daniel.akoltsev@yandex.ru>
2016-11-02 11:11:45 +0200

    faktorial add

---
+++ GroupProject/08_Danila/501_Faktorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _501_Faktorial
+{
+    class Program
+    {
+        static long Fact(uint x)//рекурсия
+        {
+                if (x < 1)
+                    return 1;
+                return x * Fact(x - 1);
+            }
+        
+        static void Main()
+        {
+            Console.WriteLine("                  Программа 'факториал числа'");
+            Console.WriteLine("            =====================================");
+            Console.WriteLine(" ");
+            Console.Write("Введите число x=");
+            string x = (Console.ReadLine());
+            var f = Fact(Convert.ToUInt32(x));
+            Console.WriteLine("{0}!={1}", x, f);
+            Console.ReadLine();
+        }
+    }
+}
