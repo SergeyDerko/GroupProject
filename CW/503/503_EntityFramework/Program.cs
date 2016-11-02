b7043ee91ec58e9fc59efa594ef30da03e50b950
mihail <mihail_orlov@hotmail.com>
2016-11-02 11:39:38 +0200

    Add entity framework

--- CW/503/503_EntityFramework/Program.cs
+++ CW/503/503_EntityFramework/Program.cs
@@ -1,12 +1,31 @@
 ﻿//материал взят с сайта http://professorweb.ru/my/entity-framework/6/level1/
 
+using System;
+using _503_EntityFramework.Model;
+
 namespace _503_EntityFramework
 {
     class Program
     {
         static void Main()
         {
+            // Создать объект для записи в БД
+            var customer = new Customer
+            {
+                FirstName = "Вася",
+                LastName = "Пупкин",
+                Age = 27,
+                City = "Москва",
+                BirthDate = DateTime.Now.AddYears(-27)
+            };
 
+            // Создать объект контекста
+            using (var context = new SampleContext())
+            {
+                // Вставить объект в БД и сохранить изменения
+                context.Customers.Add(customer);
+                context.SaveChanges();
+            }
         }
     }
 }
--- cw/503/503_EntityFramework/Model/Customer.cs
+++ cw/503/503_EntityFramework/Model/Customer.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace _503_EntityFramework.Model
 {
     public class Customer
     {
         // Определить поля, которые используются в базе данных
-        
+        [Key]
         public int IdCustomer { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
