48424edb7c3a51863d7cbba270f7587405428c30
@Pikhota <s.pikhota@gmail.com>
2016-11-04 21:20:23 +0200

    add database

--- 03_Oleg/EntityFramework/SampleContext.cs
+++ 03_Oleg/EntityFramework/SampleContext.cs
@@ -1,12 +1,14 @@
 ﻿using System.Data.Entity;
 using EntityFramework.Model;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
 
 namespace EntityFramework
 {
-    public class SampleContext : DbContext
+    public class SampleContext : Context
     {
         public DbSet<Customer> Customers { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
+        public ISet<Order> Orders { get; set; }
     }
 }
--- 09_Sergei/503_EF/model/Order.cs
+++ 09_Sergei/503_EF/model/Order.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace _503_EF.model
 {
     class Order
     {
+        [Key]
+        public int IDOrder { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime PurchaseDate { get; set; }
     }
 }
