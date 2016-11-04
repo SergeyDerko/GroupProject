f045e21846d85ba21076f43a41ed7e902044b7ca
mihail <mihail_orlov@hotmail.com>
2016-11-04 17:55:55 +0200

    add connent, command and reader

--- CW/504/504_ado.net/Program.cs
+++ CW/504/504_ado.net/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace _504_ado.net
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace _504_ado.net
 {
     class Program
     {
         static void Main()
         {
+            string connectionString = @"data source=(LocalDb)\MSSQLLocalDB;Initial Catalog=TestNewEntityDataBase;Integrated Security=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "select * from Customer";
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var table = new DataTable();
+                        table.Load(reader);
+                    }
+                }
+            }
         }
     }
 }
