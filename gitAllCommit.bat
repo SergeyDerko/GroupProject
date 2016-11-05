b08389712582b4dee605c51fd0ef6288f35e5fff
mihail <mihail_orlov@hotmail.com>
2016-11-05 23:53:58 +0200

    Change batch file for commit

--- gitAllCommit.bat
+++ gitAllCommit.bat
@@ -1,4 +1,7 @@
 git pull
 git add .
 git commit
+: для записи комментария через стороннюю программу нужно добавить следующую запись в .gitconfig
+:[core]
+:	editor = 'C:\\prg\\TCPU67\\Programm\\Notepad++\\notepad++.exe' -multiInst -notabbar -nosession -noPlugin
 git push
