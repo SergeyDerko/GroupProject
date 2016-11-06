ead6056fed4d9976f2e454e7a5b0cf78ddbe137a
voooo <voooo@DESKTOP>
2016-11-06 23:07:11 +0200

    fix

--- 05_Eugene/_DIR_FILE_/Program.cs
+++ 05_Eugene/_DIR_FILE_/Program.cs
@@ -11,10 +11,26 @@ namespace _DIR_FILE_
     {
         static void Main(string[] args)
         {
-            string path =@"c:/download";
-            string myFile;
-            myFile=Directory.GetFiles(path, "*.avi").ToString();
-            Console.WriteLine(myFile);
+            string path =@"c:\download";
+            string [] myFile;
+            File.WriteAllText(@"c:\download\list.txt","");
+            myFile =Directory.GetFiles(path,"*.avi");
+            //foreach (string dir in myFile)
+            //{
+            //    Console.WriteLine(dir);
+            //}
+            for (int i = 0; i < myFile.Length; i++)
+            {
+                var s = myFile[i].Substring((path.Length+1), myFile[i].Length -(path.Length+1)) + Environment.NewLine;
+                File.AppendAllText(@"c:\download\list.txt", s);
+            }
+            //foreach (var s in myFile)
+            //{
+                //s.Substring(0, 11);
+             //   File.AppendAllText(@"c:\download\list.txt", s+"\n");
+                //File.AppendAllText(@"c:\download\list.txt","\n");
+            //}
+
             Console.ReadKey();
         }
     }
