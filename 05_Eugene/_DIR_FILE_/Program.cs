dea80ada0f0a1bc819202aa372f0794758a63e6d
voooo <voooo@DESKTOP>
2016-11-06 21:41:21 +0200

    fix

--- 05_Eugene/_DIR_FILE_/Program.cs
+++ 05_Eugene/_DIR_FILE_/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,12 @@ namespace _DIR_FILE_
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
+            string path =@"c:/download";
+            string myFile;
+            myFile=Directory.GetFiles(path, "*.avi").ToString();
+            Console.WriteLine(myFile);
+            Console.ReadKey();
         }
     }
 }
