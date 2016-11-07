46284cba0adfb6e5db9535c3472df69912c9d23c
voooo <voooo@DESKTOP>
2016-11-07 20:07:07 +0200

    add exam

---
+++ 05_Eugene/PART1_EXAM_DIR_SRV/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PART1_EXAM_DIR_SRV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
