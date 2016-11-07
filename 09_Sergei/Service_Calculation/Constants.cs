0164e74a30aeb928c554ae3ffce8452b9b614840
@Pikhota <s.pikhota@gmail.com>
2016-11-07 12:17:43 +0200

    add project CalculationService

---
+++ 09_Sergei/Service_Calculation/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Service_Calculation
+{
+    internal class Constants
+    {
+        /// <summary>
+        /// Интервал проверки флага завершения работы службы.
+        /// </summary>
+        public static int TimeScanStopParam = 500;
+    }
+}
---
+++ 09_Sergei/Service_Calculation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace Service_Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += UnhandledExceptionHandler;
+
+            var svc = new MainService();
+            if (Array.IndexOf(args, "console") != -1 || Array.IndexOf(args, "c") != -1)
+            {
+                svc.StartSvc();
+                Console.WriteLine("Press a key for exit...");
+                Console.ReadKey(true);
+                svc.StopSvc();
+            }
+            else
+            {
+                ServiceBase.Run(svc);
+            }
+        }
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            const string method = "UnhandledExceptionHandler";
+            var ex = (Exception)args.ExceptionObject;
+            Console.WriteLine(ex == null ? "Error!" : $"{method}\n{ex}");
+        }
+    }
+}
+
