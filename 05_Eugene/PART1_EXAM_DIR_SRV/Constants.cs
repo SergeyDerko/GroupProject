db3e4ea9f2c7ba8a77ab8ff8a908e691102aa81a
voooo <voooo@DESKTOP>
2016-11-08 12:11:23 +0200

    add

---
+++ 05_Eugene/PART1_EXAM_DIR_SRV/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace PART1_EXAM_DIR_SRV
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Интервал проверки флага завершения работы службы.
+        /// </summary>
+        public static int TimeScanStopParam = 500;
+    }
+}
