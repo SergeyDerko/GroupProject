641abeeaec59041e811fe84164c14dcd189f6a4c
mihail <mihail_orlov@hotmail.com>
2016-11-09 10:29:12 +0200

    fix

--- CW/503/503_EntityFramework/SampleContext.cs
+++ CW/503/503_EntityFramework/SampleContext.cs
@@ -1,11 +1,10 @@
-﻿using _503_EntityFramework.Model;
+﻿using System.Data.Entity;
+using _503_EntityFramework.Model;
 
 namespace _503_EntityFramework
 {
     public class SampleContext : DbContext
     {
         public DbSet<Customer> Customers { get; set; }
-
-        public DbSet<Order> Orders { get; set; }
     }
 }
