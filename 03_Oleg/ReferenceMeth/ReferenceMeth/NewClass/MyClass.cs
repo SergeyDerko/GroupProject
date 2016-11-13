e700e2fb14b1f37218fbc984efc7cc713015878b
OLEG <OLEG@OLEG-PC>
2016-11-13 23:45:38 +0200

    FIX + NEW

---
+++ 03_Oleg/ReferenceMeth/ReferenceMeth/NewClass/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReferenceMeth.NewClass
+{
+    class MyClass
+    {
+        int alpha, beta;
+
+        public MyClass (int i, int j)
+        {
+            alpha = i;
+            beta = j;
+        }
+        // Возратить значение  true, если параметр ob
+        // имеет те же значения, что и вызывающий объект.
+        public bool SameAs(MyClass ob)
+        {
+            if ((ob.alpha == alpha) & (ob.beta == beta))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Сделать копию объекта ob.
+        public void Copy (MyClass ob)
+        {
+            alpha = ob.alpha;
+            beta = ob.beta;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("alpha: {0}, beta: {1}", alpha, beta);
+        }
+    }
+}
---
+++ 03_Oleg/ReferenceMeth/ReferenceMeth/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReferenceMeth.NewClass;
+
+namespace ReferenceMeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ob1 = new MyClass(4, 5);
+            MyClass ob2 = new MyClass(6, 7);
+
+            Console.Write("ob: ");
+            ob1.Show();
+
+            Console.Write("ob: ");
+            ob2.Show();
+
+            if (ob1.SameAs(ob2))
+            {
+                Console.WriteLine("ob1 и ob2 имеют одинаковые значения.");
+            }
+            else
+            {
+                Console.WriteLine("ob1 и ob2 имеют разные значения.");
+            }
+
+            Console.WriteLine();
+
+            // А теперь сделать объект ob1 копией объекта ob2.
+            ob1.Copy(ob2);
+
+            Console.Write("ob1 после копирования: ");
+            ob1.Show();
+
+            if(ob1.SameAs(ob2))
+            {
+                Console.WriteLine("ob1 и ob2 имеют одинаковые значения.");
+            }
+            else
+            {
+                Console.WriteLine("ob1 и ob2 имеют разные значения.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
