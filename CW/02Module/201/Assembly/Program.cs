f226d2a5c80b2656742cad1ab448e26a744ab03f
mihail <mihail_orlov@hotmail.com>
2016-11-27 18:13:50 +0200

    fix conflict

--- CW/02Module/201/Assembly/Program.cs
+++ CW/02Module/201/Assembly/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Assembly
+﻿using Lib1User = Lib1.User;
+using Lib2User = Lib2.User;
+
+namespace Assembly
 {
     class Program
     {
         static void Main()
         {
-            var user1 = new Lib1.User();
-            var user2 = new Lib2.User();
+            var user1 = new Lib1User();
+            var user2 = new Lib2User();
         }
     }
 }
