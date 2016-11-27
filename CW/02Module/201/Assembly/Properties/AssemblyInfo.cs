b9686a16ab1d0f6bd197a8460e3963eb5f8b3284
mihail <mihail_orlov@hotmail.com>
2016-11-27 17:46:13 +0200

    change assembly information

--- CW/02Module/201/Assembly/Properties/AssemblyInfo.cs
+++ CW/02Module/201/Assembly/Properties/AssemblyInfo.cs
@@ -1,17 +1,17 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assembly")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assembly")]
+[assembly: AssemblyTitle("AssemblyTitle")]
+[assembly: AssemblyDescription("AssemblyDescription")]
+[assembly: AssemblyConfiguration("AssemblyConfiguration")]
+[assembly: AssemblyCompany("AssemblyCompany")]
+[assembly: AssemblyProduct("AssemblyProduct")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("AssemblyTrademark")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("uk-UA")]
+
