18177504a51181892df6958ca31f12c5f72cbd48
yurikhub <yurik.matviychuk@gmail.com>
2016-12-03 01:06:07 +0200

    big update

---
+++ 06_Jury/Genarate_And_Calculate_Services/CalcMathExpressionService/service/CalculationService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using CalcMathExpressionService.modal;
+
+namespace CalcMathExpressionService.service
+{
+    /// <summary>
+    /// Основной код сервиса для формирования задач в ис-про
+    /// </summary>
+    internal class CalculationService
+    {
+        private volatile bool _stopFlag;
+        private Thread _thread;
+
+        public void Start()
+        {
+            _thread = new Thread(x =>
+            {
+                var stopFlag = _stopFlag;
+                do
+                {
+                    Scan.StartCalculation(); // старт
+
+                } while (!SrvUtils.Retarder(5, ref stopFlag));
+            });
+            _thread.Start();
+        }
+
+        public void Stop()
+        {
+            _stopFlag = true;
+        }
+    }
+}
---
+++ 06_Jury/Genarate_And_Calculate_Services/CalcMathExpressionService/service/SrvInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.ServiceProcess;
+
+
+namespace CalcMathExpressionService.service
+{
+    [RunInstaller(true)]
+    public class SrvInstaller : Installer
+    {
+        private readonly ServiceProcessInstaller _serviceProcessInstaller;
+        private readonly ServiceInstaller _serviceInstaller;
+
+        public SrvInstaller()
+        {
+            _serviceProcessInstaller = new ServiceProcessInstaller();
+            _serviceInstaller = new ServiceInstaller();
+
+            _serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+
+            _serviceInstaller.ServiceName = "Part1_exam_srv_" + Path.GetFileNameWithoutExtension(typeof(Program).Assembly.CodeBase);
+            _serviceInstaller.DisplayName = "Part1_exam_srv_" + Path.GetFileNameWithoutExtension(typeof(Program).Assembly.CodeBase);
+            _serviceInstaller.Description = "Часть 1. Основы C#. Cервис - Калькулятор арифметических выражений считанных с файла";
+            _serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            Installers.AddRange(new Installer[] { _serviceProcessInstaller, _serviceInstaller });
+        }
+    }
+}
