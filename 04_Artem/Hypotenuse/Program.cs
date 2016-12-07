7a52bf7b67bb7e380dd3affe9f5cd23abaf7ea91
artemanikeiev <artemanikeiev@gmail.com>
2016-12-07 19:33:56 +0200

    Add files via upload
    hypotenuse

---
+++ 04_Artem/Hypotenuse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hypotenuse
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first cathetus length:");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second cathetus length:");
+            var y = Convert.ToInt32(Console.ReadLine());
+
+            var z = Math.Sqrt(x*x +y*y);
+      
+            Console.Write("The hypothenuse of the triangle with cathetuses " + x);
+            Console.Write(" and " + y);
+            Console.Write(" equals " + z);
+        
+            Console.ReadKey();
+        }
+    }
+}
