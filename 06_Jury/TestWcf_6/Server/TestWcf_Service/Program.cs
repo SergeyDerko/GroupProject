26ce9dfe122945534be6f2de8eb765bed1314423
yurikhub <yurik.matviychuk@gmail.com>
2016-12-09 20:02:41 +0200

    ref testwcf service

--- 06_Jury/TestWcf_6/Server/TestWcf_Service/Program.cs
+++ 06_Jury/TestWcf_6/Server/TestWcf_Service/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using TestWcf_Service.service;
 
 namespace TestWcf_Service
 {
@@ -10,6 +14,56 @@ namespace TestWcf_Service
     {
         static void Main(string[] args)
         {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += UnhandledExceptionHandler;
+            Initlog();
+            var svc = new MainService();
+            if (Array.IndexOf(args, "console") != -1 || Array.IndexOf(args, "c") != -1)
+            {
+                svc.StartSvc();
+                Console.WriteLine("Press a key for exit...");
+                Console.ReadKey(true);
+                svc.StopSvc();
+               
+            }
+            else
+            {
+                ServiceBase.Run(svc);
+            }
         }
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            const string method = "UnhandledExceptionHandler";
+            var ex = (Exception)args.ExceptionObject;
+            Console.WriteLine(ex == null ? "Error!" : $"{method}\n{ex}");
+        }
+
+        private static void Initlog()
+        {
+            #region Инициализация логгера
+
+            Logger.Level = Config.Get.Log.Level;
+
+            // Определение пути доступа к журналу событий
+
+            try
+            {
+                Logger.Dir = Config.Get.Log.Dir;
+                Directory.CreateDirectory(Logger.Dir); // если каталог не существует - создаст
+            }
+            catch
+            {
+
+            }
+
+            Logger.Prefix = Config.Get.Log.Prefix;
+            Logger.Start();
+            Logger.Write(Level.Info, "Старт сервера");
+
+            #endregion
+
+        }
+
+
     }
 }
---
+++ 06_Jury/TestWcf_6/Server/TestWcf_Service/service/TestWcfService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+
+namespace TestWcf_Service.service
+{
+    /// <summary>
+    /// Основной код сервиса для формирования задач в ис-про
+    /// </summary>
+    internal class TestWcfService
+    {
+        private volatile bool _stopFlag;
+        private Thread _thread;
+
+        public void Start()
+        {
+            _thread = new Thread(x =>
+            {
+                var stopFlag = _stopFlag;
+                do
+                {
+                    //Generator.Generate(); // старт
+
+                } while (!SrvUtils.Retarder(5, ref stopFlag));
+            });
+            _thread.Start();
+            //Process.Start(Generator.FolderForFiles); // открыть папку с файлами
+        }
+
+        public void Stop()
+        {
+            _stopFlag = true;
+        }
+    }
+}
