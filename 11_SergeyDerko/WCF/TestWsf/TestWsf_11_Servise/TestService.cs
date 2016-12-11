e6477cdd37a8a0dd9f7031eaea253f6485bb6159
SergeyDerko <sergeyderko1986@yandex.ru>
2016-12-11 21:47:02 +0200

    Servise 11

---
+++ 11_SergeyDerko/WCF/TestWsf/TestWsf_11_Servise/TestService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace TestWsf_11_Servise
+{
+    /// <summary>
+    /// Основной код сервиса для формирования задач в ис-про
+    /// </summary>
+    class TestService
+    {
+        private volatile bool _stopFlag;
+        private Thread _thread;
+        private volatile bool _processingError;
+
+        public void Start()
+        {
+            _thread = new Thread(x =>
+            {
+                do
+                {
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        Console.WriteLine($"TestService: {i}");
+                        SrvUtils.Retarder(1, ref _stopFlag);
+                    }
+                } while (!SrvUtils.Retarder(30, ref _stopFlag));
+            });
+            _thread.Start();
+        }
+
+        public void Stop()
+        {
+            _stopFlag = true;
+        }
+    }
+}
