ace55b331c8bfd77612aab63f2771be4dd0907ab
@Pikhota <s.pikhota@gmail.com>
2016-12-14 18:59:59 +0200

    add TestWCF_09_Service

---
+++ 09_Sergei/TestWCF_09/Server/TestWCF_09_Service/ConstantsWCF.cs
@@ -0,0 +1,10 @@
+﻿namespace TestWCF_09_Service
+{
+    internal class ConstantsWCF
+    {
+        /// <summary>
+        /// Интервал проверки флага завершения работы службы.
+        /// </summary>
+        public static int TimeScanStopParam = 500;
+    }
+}
