0fe69e9167a915bc379d924caeda70aeb2a413e3
yurikhub <yurik.matviychuk@gmail.com>
2016-12-15 09:46:50 +0200

    add new wcfservice

---
+++ 06_Jury/TestWcf6/Common/TestWcf6Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWcf6Common
+{
+    public class Class1
+    {
+    }
+}
