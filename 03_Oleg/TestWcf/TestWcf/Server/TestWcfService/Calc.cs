4d4c67f225479ef4e9f0936e4bdd2cfe304e72a6
OLEG <OLEG@OLEG-PC>
2016-12-16 18:47:48 +0200

    new

---
+++ 03_Oleg/TestWcf/TestWcf/Server/TestWcfService/Calc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Common;
+
+namespace Common
+{
+    public class Calc : ICalc
+    {
+        public int Sum(int a, int b)
+        {
+            var sum = a + b;
+            Console.WriteLine($"{a} + {b} = {sum}");
+            return sum;
+        }
+    }
+}
---
+++ 03_Oleg/TestWcf/TestWcf/TestWcfNewService/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using Common;
+
+namespace TestWcfService
+{
+    class Program
+    {
+        static void Main()
+        {
+            var address = new Uri("http://localhost:555/ICalc");
+            var binding = new BasicHttpBinding();
+            var contract = typeof(ICalc);
+
+            var host = new ServiceHost(typeof(Calc));
+            host.AddServiceEndpoint(contract, binding, address);
+            host.Open();
+
+            Console.WriteLine("Сервер запущен");
+            Console.ReadKey();
+        }
+    }
+}
