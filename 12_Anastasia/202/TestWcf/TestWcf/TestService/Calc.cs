bba0e0dff33cd768a4b0c16203515414cbe138dd
Anastasia Nepomiluyev <nepomiluyev@gmail.com>
2016-12-25 18:31:06 +0200

    my service

--- 12_Anastasia/202/TestWcf/TestWcf/TestService/Calc.cs
+++ 12_Anastasia/202/TestWcf/TestWcf/TestService/Calc.cs
@@ -6,28 +6,34 @@ using System.Threading.Tasks;
 
 namespace TestService
 {
-    internal class Calc : TestWcfCommon.ICalc
+    internal class Calc : Common.ICalc
     {
         public int Division(int a, int b)
         {
             if (b == 0)
                 return -Int32.MaxValue;
             else
-                return a / b;      
+            {
+                Console.WriteLine($"{a} / {b} = {a/b}");
+                return a/b;
+            }
         }
 
         public int Multiplication(int a, int b)
         {
+            Console.WriteLine($"{a} * {b} = {a*b}");
             return a*b;
         }
 
         public int Substraction(int a, int b)
         {
+            Console.WriteLine($"{a} - {b} = {a-b}");
             return a - b;
         }
 
         public int Sum(int a, int b)
         {
+            Console.WriteLine($"{a} + {b} = {a+b}");
             return a + b;
         }
     }
--- 12_Anastasia/202/TestWcf/TestWcf/TestService/Notifier.cs
+++ 12_Anastasia/202/TestWcf/TestWcf/TestService/Notifier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Channels;
 using Common.TestWcfCommon;
-using TestWcfCommon;
+using Common;
 
 namespace TestService
 {
