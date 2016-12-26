7e973da1c20cd8afcb4d6ecb25bdb19197f971a8
@Pikhota <s.pikhota@gmail.com>
2016-12-26 19:45:41 +0200

    add SergeyPekhota

---
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/SergeyPekhotaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWcfSite.Controllers
+{
+    public class SergeyPekhotaController : Controller
+    {
+        // GET: SergeyPekhota
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
