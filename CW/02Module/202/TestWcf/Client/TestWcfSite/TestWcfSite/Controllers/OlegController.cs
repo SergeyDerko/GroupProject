fd5b0a318868c5b4e9a08458343081951e6697fa
OLEG <OLEG@OLEG-PC>
2016-12-28 00:04:26 +0200

    i try

--- CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/OlegController.cs
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/OlegController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +9,28 @@ namespace TestWcfSite.Controllers
 {
     public class OlegController : Controller
     {
+        protected List<string> RowInts;
         // GET: Oleg
         public ActionResult Index()
         {
+            ViewBag.First = MultResult();
+            return View();
+        }
+
+        public ActionResult MultResult()
+        {
+            string mess;
+            for (int i = 1, m; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    m = i*j;
+                    mess = i + " * " + j + " = " + m;
+                    RowInts.Add(mess);
+                    m = 0;                     
+                }
+            }
+            ViewBag.Mess = RowInts;
             return View();
         }
     }
