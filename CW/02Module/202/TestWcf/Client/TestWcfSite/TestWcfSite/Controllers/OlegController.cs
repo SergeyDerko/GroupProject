aa7997f8004ffecaa193c3abf1579bcadea74a0b
OLEG <OLEG@OLEG-PC>
2016-12-28 00:38:11 +0200

    try new

--- CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/OlegController.cs
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/OlegController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,7 @@ namespace TestWcfSite.Controllers
 {
     public class OlegController : Controller
     {
-        protected List<string> RowInts;
+        protected List<mess> RowInts;
         // GET: Oleg
         public ActionResult Index()
         {
@@ -19,19 +20,39 @@ namespace TestWcfSite.Controllers
 
         public ActionResult MultResult()
         {
-            string mess;
             for (int i = 1, m; i <= 10; i++)
             {
                 for (int j = 1; j <= 10; j++)
                 {
                     m = i*j;
-                    mess = i + " * " + j + " = " + m;
-                    RowInts.Add(mess);
-                    m = 0;                     
+                    mess newWord = new mess(i, j, m);
+                    newWord.GO();
+                    RowInts.Add(newWord);
+                    m = 0;                    
                 }
             }
             ViewBag.Mess = RowInts;
             return View();
+        }
+    }
+
+    public class mess
+    {
+        public int first;
+        public int second;
+        public int third;
+        public string newM;
+
+        public mess(int a, int b, int c)
+        {
+            first = a;
+            second = b;
+            third = c;
+        }
+
+        internal void GO()
+        {
+            newM = first + " * " + second + " = " + third;
         }
     }
 }
