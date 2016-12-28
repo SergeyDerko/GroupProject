57acba7dd5af64a306db1159a011859415088316
@Pikhota <s.pikhota@gmail.com>
2016-12-28 14:50:52 +0200

    undo changes

---
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Models/Multiply.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TestWcfSite.Models
+{
+    public class Multiply
+    {
+        public int Value { get; set; }
+        public int Count { get; set; }
+        public int StartIndex { get; set; }
+
+        public Multiply()
+        {
+        }
+
+        public Multiply(int value, int count, int startIndex)
+        {
+            Value = value;
+            Count = count;
+            StartIndex = startIndex;
+        }
+
+
+        public List<string> Multy()
+        {
+
+            var list = new List<string>();
+            StartIndex = 2;
+            var str = new StringBuilder();
+            for (var i = StartIndex; i < Count+StartIndex; i++)
+            {
+                str.Append($"{Value}x{i}={Value * i}</br>");
+                list.Add(str.ToString());
+            }
+            
+            return list;
+        }
+    }
+}
