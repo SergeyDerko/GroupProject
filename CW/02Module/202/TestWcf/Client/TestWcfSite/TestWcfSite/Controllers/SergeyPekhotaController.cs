e1f9a7b2617ab3e7f9912b7a60e6d8986c4128ac
@Pikhota <s.pikhota@gmail.com>
2017-01-04 16:06:38 +0200

    update, change logic, delete excess class(model)

--- CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/SergeyPekhotaController.cs
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Controllers/SergeyPekhotaController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
 using System.Web.UI.HtmlControls;
 using TestWcfSite.Models.PikhotaSerhiiModels;
 
@@ -7,14 +9,9 @@ namespace TestWcfSite.Controllers
     public class SergeyPekhotaController : Controller
     {
         // GET: SergeyPekhota
-        public ActionResult Index(int value = 2,int index=1,int count=9)
+        public ActionResult Index()
         {
-            var calc = new Calculator
-            {
-                Value = value,
-                StartIndex = index,
-                Count = count
-            };
+            var calc = new Calculator();
             return View(calc);
         }
 
@@ -24,28 +21,120 @@ namespace TestWcfSite.Controllers
             return View("Index",calc);
         }
 
-        public ActionResult MultiplyTable(TablesOfMathActions table)
+        public ActionResult MultiplyTable()
         {
+
+            ViewBag.Begintd = "<td class = \"tb-pikhota \">";
+            ViewBag.Endtd = "</td>";
             ViewBag.Counter = 0;
-            return View(table);
+            ViewBag.TagBr = "<br />";
+            ViewData.Model = MultyTables();
+            return View();
+        }
+        
+
+        public ActionResult DivisionTable()
+        {
+            ViewBag.Begintd = "<td class = \"tb-pikhota \">";
+            ViewBag.Endtd = "</td>";
+            ViewBag.Counter = 0;
+            ViewBag.TagBr = "<br />";
+            ViewData.Model = DivTables();
+            return View();
         }
 
-        public ActionResult DivisionTable(TablesOfMathActions table)
+        public ActionResult SumTable()
         {
+            ViewBag.Begintd = "<td class = \"tb-pikhota \">";
+            ViewBag.Endtd = "</td>";
             ViewBag.Counter = 0;
-            return View(table);
+            ViewBag.TagBr = "<br />";
+            ViewData.Model = SubTables();
+            return View();
         }
 
-        public ActionResult SumTable(TablesOfMathActions table)
+        public ActionResult SubstractionTable()
         {
+            ViewBag.Begintd = "<td class = \"tb-pikhota \">";
+            ViewBag.Endtd = "</td>";
             ViewBag.Counter = 0;
-            return View(table);
+            ViewBag.TagBr = "<br />";
+            ViewData.Model = SubTables();
+            return View();
         }
 
-        public ActionResult SubstractionTable(TablesOfMathActions table)
+        #region ActionMathMethods
+        public List<string> MultyTables()
         {
-            ViewBag.Counter = 0;
-            return View(table);
+            var list = new List<string>();
+            var str = new StringBuilder();
+            for (var i = 2; i < 10; i++)
+            {
+                for (var j = 2; j < 10; j++)
+                {
+                    str.Append($"{i}x{j}={i * j}");
+                    list.Add(str.ToString());
+                    str.Clear();
+                }
+            }
+            return list;
         }
+
+        public List<string> DivTables()
+        {
+            var list = new List<string>();
+            var str = new StringBuilder();
+            for (var i = 2; i < 10; i++)
+            {
+                for (var j = 2; j < 10; j++)
+                {
+
+                    var result = i % j == 0
+                        ? str.Append($"{i}/{j}={i / j}")
+                        : str.Append($"{i}/{j}={((double)i / (double)j):N2}");
+                    list.Add(result.ToString());
+                    str.Clear();
+                }
+
+            }
+            return list;
+        }
+
+        public List<string> SumTables()
+        {
+            var list = new List<string>();
+            var str = new StringBuilder();
+            for (var i = 2; i < 10; i++)
+            {
+                for (int j = 2; j < 10; j++)
+                {
+
+                    str.Append($"{i}+{j}={i + j}");
+                    list.Add(str.ToString());
+                    str.Clear();
+                }
+            }
+            return list;
+        }
+
+        public List<string> SubTables()
+        {
+            var list = new List<string>();
+            var str = new StringBuilder();
+            for (var i = 2; i < 10; i++)
+            {
+                for (int j = 2; j < 10; j++)
+                {
+
+                    str.Append($"{i}-{j}={i - j}");
+                    list.Add(str.ToString());
+                    str.Clear();
+                }
+
+            }
+            return list;
+        }
+        #endregion
+
     }
 }
