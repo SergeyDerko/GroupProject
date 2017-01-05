a864966a79cc656524562eb4c5885810a3afc1da
yurikhub <yurik.matviychuk@gmail.com>
2017-01-05 21:42:39 +0200

    add class, interface

---
+++ CW/02Module/202/TestWcf/Server/Yuriy/ExchangeRatesServiceLib/ExchangeRates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesServiceLib
+{
+    public class ExchangeRates:IExchangeRates
+    {
+        public string Usd { get; set; }
+        public string Eur { get; set; }
+    }
+}
