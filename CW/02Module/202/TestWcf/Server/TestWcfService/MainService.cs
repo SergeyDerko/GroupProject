7ceed70758633be35ef3e245e0d8808bcee16183
Basterkeel <daniel.akoltsev@yandex.ru>
2017-01-08 23:23:58 +0200

    add Plitka

--- CW/02Module/202/TestWcf/Server/TestWcfService/MainService.cs
+++ CW/02Module/202/TestWcf/Server/TestWcfService/MainService.cs
@@ -1,5 +1,6 @@
 ﻿using System.ServiceProcess;
 using ExchangeRatesServiceLib;
+using PlitkaLib;
 using SergeyDerkoLibrary;
 using TestWcfLib;
 
@@ -10,12 +11,14 @@ namespace TestWcfService
         private readonly BaseService<Calc> _calcService = new BaseService<Calc>();
         private readonly BaseService<ScanPc> _scanPcService = new BaseService<ScanPc>();
         private readonly BaseService<ExchangeRates> _exchangeRates = new BaseService<ExchangeRates>();
+        private readonly BaseService<Plitka> _plitka = new BaseService<Plitka>();
 
         public void StartSvc()
         {
             _calcService.Start();
             _scanPcService.Start();
             _exchangeRates.Start();
+            _plitka.Start();
         }
 
         public void StopSvc()
@@ -23,6 +26,7 @@ namespace TestWcfService
             _scanPcService.Stop();
             _calcService.Stop();
             _exchangeRates.Stop();
+            _plitka.Stop();
         }
 
         protected override void OnStart(string[] args)
