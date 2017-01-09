a4aaaa6d5f3d50e122303504cdaf6bc055bdbeda
@Pikhota <s.pikhota@gmail.com>
2017-01-09 12:31:15 +0200

    update

--- CW/02Module/202/TestWcf/Server/Pikhota/CurrencyConvertWcfLib/CurrencyConvert.cs
+++ CW/02Module/202/TestWcf/Server/Pikhota/CurrencyConvertWcfLib/CurrencyConvert.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CurrencyConvertLib;
 
 namespace CurrencyConvertWcfLib
 {
-    public class CurrencyConvert
+    public class CurrencyConvert : ICurrencyConvert
     {
+        public string Usd = "USD";
+        public string Euro = "EUR";
+        public string Uah = "UAH";
+        public decimal ChangeCurrency(string fromCurrency, string toCurrency)
+        {
+            decimal result = 0;
+            if (fromCurrency == Usd)
+            {
+                
+            }
+            return result;
+        }
     }
 }
---
+++ CW/02Module/202/TestWcf/Server/Pikhota/CurrencyConvertWcfLib/ICurrencyConvert.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace CurrencyConvertLib
+{
+    [ServiceContract]
+    public interface ICurrencyConvert
+    {
+        [OperationContract]
+        decimal ChangeCurrency(string fromCurrency, string toCurrency);
+    }
+}
