e61eabd9c2f3967c6e78c6577fc5d87e003b96df
aloha <aloha-an@yandex.ru>
2017-01-15 18:24:22 +0200

    Edit model

--- CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Models/AlekseyAntonov/ServicesVeawerModel.cs
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Models/AlekseyAntonov/ServicesVeawerModel.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using ServicesVeawerClientLib.ServicesVeawerServiceReference;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace TestWcfSite.Models.AlekseyAntonov
 {
     public class ServicesVeawerModel
     {
+       public ServicesVeawerModel()
+        {
+            ServicesVeawerClient client = new ServicesVeawerClient();
+            List<Service> servicesList = new List<Service>();
+            var temp=client.ServicesSearcher();
+
+            foreach (var t in temp)
+            {
+                servicesList.Add(t);
+            }
+            client.Close();
+        } 
     }
 }
