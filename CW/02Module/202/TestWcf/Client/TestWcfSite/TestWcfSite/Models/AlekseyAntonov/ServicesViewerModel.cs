f2d92de685921f16f9a3434069664b079d1fe8b3
aloha <aloha-an@yandex.ru>
2017-01-17 22:52:19 +0200

    Edit my client

--- CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Models/AlekseyAntonov/ServicesViewerModel.cs
+++ CW/02Module/202/TestWcf/Client/TestWcfSite/TestWcfSite/Models/AlekseyAntonov/ServicesViewerModel.cs
@@ -5,27 +5,35 @@ namespace TestWcfSite.Models.AlekseyAntonov
 {
     public class ServicesViewerModel 
     {
-
-        public  string BehaviorName { get; set; }
+        public string BehaviorName { get; set; }
         public string ServiceBehaviorConfiguration { get; set; }
         public string ServiceName { get; set; }
-        public  string EndpointBinding { get; set; }
-        public  string EndpointContract { get; set; }
+        public string EndpointBinding { get; set; }
+        public string EndpointContract { get; set; }
         public string BaseAddress { get; set; }
-        public int CountModels { get; set; }
+
+        // public static ServicesViewerClient ServicesView { get; set; }
+
+        public ServicesViewerModel(){}
+
         public ServicesViewerModel(int num)
         {
-          //  new Service();
-            var serviceView = new ServicesViewerClient();
-            this.CountModels = serviceView.ServicesSearcher().Length;
-            OneService temp = serviceView.ServicesSearcher()[num];
-            this.BehaviorName = temp.BehaviorName;
-            this.BaseAddress = temp.BaseAddress;
-            this.EndpointBinding = temp.EndpointBinding;
-            this.EndpointContract = temp.EndpointContract;
-            this.ServiceBehaviorConfiguration = temp.ServiceBehaviorConfiguration;
-            this.ServiceName = temp.ServiceName;
-            serviceView.Close();
+            var servicesView = new ServicesViewerClient();
+            OneService temp = servicesView.ServicesSearcher()[num];
+            BehaviorName = temp.BehaviorName;
+            BaseAddress = temp.BaseAddress;
+            EndpointBinding = temp.EndpointBinding;
+            EndpointContract = temp.EndpointContract;
+            ServiceBehaviorConfiguration = temp.ServiceBehaviorConfiguration;
+            ServiceName = temp.ServiceName;
+            servicesView.Close();
+        }
+        public int AmountService()
+        {
+            var servicesView = new ServicesViewerClient();
+            int amount = servicesView.ServicesSearcher().Length;
+            servicesView.Close();
+            return amount;
         }
     }
 }
