389704ef53987f917e151ca0d1adfaf3ac7228f8
mihail <mihail_orlov@hotmail.com>
2017-01-17 11:33:59 +0200

    refactoring

---
+++ CW/02Module/202/TestWcf/Server/TestWcfLib/Expression.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace TestWcfLib
+{
+    [DataContract]
+    public class Expression
+    {
+        [DataMember]
+        public decimal A { get; set; }
+
+        [DataMember]
+        public decimal B { get; set; }
+
+        [DataMember]
+        public MathAction MathAction { get; set; }
+    }
+    public enum MathAction
+    {
+        Sum = 1,
+        Min = 2
+    }
+}
---
+++ CW/02Module/202/TestWcf/Server/TestWcfLib/ICalc.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace TestWcfLib
+{
+    [ServiceContract]
+    public interface ICalc
+    {
+        [OperationContract]
+        int Sum(int a, int b);
+
+        [OperationContract]
+        decimal Execute(Expression expression);
+
+        [OperationContract]
+        decimal Execute(decimal a, decimal b, MathAction mathAction);
+    }
+}
