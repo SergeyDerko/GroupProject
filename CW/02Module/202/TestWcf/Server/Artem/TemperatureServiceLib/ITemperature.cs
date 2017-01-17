baa5bfb6b2eaca734648b973ea6cd796fa4a3e7f
@Pikhota <s.pikhota@gmail.com>
2017-01-17 11:23:06 +0200

    fix / вроде ничего не удаляю:)

---
+++ CW/02Module/202/TestWcf/Server/Artem/TemperatureServiceLib/ITemperature.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace TemperatureServiceLib
+{
+    [ServiceContract]
+    public interface ITemperature
+    {
+        [OperationContract]
+
+        string Temp();
+    }
+}
