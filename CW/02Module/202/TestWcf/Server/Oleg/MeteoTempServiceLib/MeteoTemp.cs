40ee2a4d7696619f3e8c5c3eb01f31f37151d09e
crxmaged <crxmaged@gmail.com>
2017-01-19 21:52:54 +0200

    add MathMeteo in class MeteoTemp

---
+++ CW/02Module/202/TestWcf/Server/Oleg/MeteoTempServiceLib/MeteoTemp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace MeteoTempServiceLib
+{
+    public class MeteoTemp : IMeteoTemp
+    {
+        public string CurrentMeteoTemp()
+        {
+            string temp;
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString("https://www.wunderground.com/personal-weather-station/dashboard?ID=I30KYIV4");
+                temp = htmlCode;
+            }
+            return $"Temp = {temp}";
+        }
+    }
+}
