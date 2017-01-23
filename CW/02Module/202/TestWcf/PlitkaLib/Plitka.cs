d0535338e3e3335a191d61dc4e83d103fad1d0c9
Basterkeel <daniel.akoltsev@yandex.ru>
2017-01-23 20:25:10 +0200

    add map

--- CW/02Module/202/TestWcf/PlitkaLib/Plitka.cs
+++ CW/02Module/202/TestWcf/PlitkaLib/Plitka.cs
@@ -3,17 +3,18 @@
 namespace PlitkaLib
 {
     public class Plitka : IPlitka
-
-
-    {
+        {
         public int GetCountTiles(int heightWall, int widthWall, int heightTile, int widthTile)
       {
             Logger.Enter();
-                var sqrTile = heightTile * widthTile;
+            Logger.Write(Level.Debug, "Иницаилизация переменных...");
+            Logger.Write(Level.Info, "Инициализация прошла успешно!");
+            var sqrTile = heightTile * widthTile;
                 var sqrWall = heightWall * widthWall;
                 var result = sqrWall / sqrTile;
             var value = $"{sqrWall} / {sqrTile} = {result}";
-        Logger.Info(value);
+            Logger.Write(Level.Info, "Посчитали)");
+            Logger.Info(value);
                 return Logger.Leave(result);
             }
         }
