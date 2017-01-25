c5dcc1e398b49898d1c87661410abe8825e32c71
yurikhub <yurik.matviychuk@gmail.com>
2017-01-25 13:47:06 +0200

    Add database

---
+++ CW/02Module/202/TestWcf/DataBase/TestWcfDB/Models/StudentsContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace TestWcfDB.Models
+{
+    public class StudentsContext : DbContext
+    {
+        public StudentsContext() : base("DBConnection")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- CW/02Module/202/TestWcf/DataBase/TestWcfDB/Program.cs
+++ CW/02Module/202/TestWcf/DataBase/TestWcfDB/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TestWcfDB.Models;
 
 namespace TestWcfDB
 {
@@ -6,7 +7,31 @@ namespace TestWcfDB
     {
         public static void Main()
         {
-            Console.WriteLine("Test");
+            //using (var db = new StudentsContext())
+            //{
+            //    var student1 = new Student{ Id =1, Name = "Олег", Surname = "Хлопов", Age = 21,  Programlanguage = "С#"};
+            //    var student2 = new Student{ Id =2, Name = "Артем", Surname = "Аникеев", Age = 22,  Programlanguage = "С#"};
+            //    var student3 = new Student{ Id =3, Name = "Юрий", Surname = "Матвийчук", Age = 23,  Programlanguage = "С#"};
+            //    var student4 = new Student{ Id =4, Name = "Даниил", Surname = "Акольцев", Age = 24,  Programlanguage = "С#"};
+            //    var student5 = new Student{ Id =5, Name = "Сергей", Surname = "Пехота", Age = 25,  Programlanguage = "С#"};
+            //    var student6 = new Student{ Id =6, Name = "Анастасия", Surname = "Непомилуєва", Age = 26,  Programlanguage = "С#"};
+            //    var student7 = new Student{ Id =7, Name = "Сергей", Surname = "Дерко", Age = 27,  Programlanguage = "С#"};
+            //    var student8 = new Student{ Id =8, Name = "Алексей", Surname = "Антонов", Age = 28,  Programlanguage = "С#"};
+                
+            //    db.Students.Add(student1);
+            //    db.Students.Add(student2);
+            //    db.Students.Add(student3);
+            //    db.Students.Add(student4);
+            //    db.Students.Add(student5);
+            //    db.Students.Add(student6);
+            //    db.Students.Add(student7);
+            //    db.Students.Add(student8);
+            //    db.SaveChanges();
+            
+            //}
+            
+            Console.WriteLine("Completed");
+            Console.ReadKey();
         }
     }
 }
